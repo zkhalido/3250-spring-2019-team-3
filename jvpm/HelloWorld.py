29c28f5dbca611ce977b8ed1d9c4a89f5b0954de
elopezh1 <46974601+elopezh1@users.noreply.github.com>
2019-01-28 14:35:35 -0700

    Update HelloWorld.py

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -2,8 +2,10 @@ def HelloWorld():
     print("Hello world")
     print("Marc Inouye")
     print("Zack Khalidov")
-	print("Robert Kupfner")
-	print("Dominick Licciardi")
+    print("Robert Kupfner")
+    print("Dominick Licciardi")
+    print("Ezequiel Lopez")
+	
 	
 
 if '__main__' == __name__:
