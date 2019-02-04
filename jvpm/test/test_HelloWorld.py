77e44a9cf5fbe8a1137daf601680fdb8ba19641b
minouye2 <minouye2@msudenver.edu>
2019-02-04 15:25:33 -0700

    Added team roles

--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -10,11 +10,11 @@ class TestHelloWorld(unittest.TestCase):
         sys.stdout.assert_has_calls(
             [call.write('Hello world'), call.write('\n'),
             call.write('We are team 3 and our names are...\n'), call.write('\n'),
-            call.write('Marc Inouye'), call.write('\n'),
-            call.write('Zack Khalidov'), call.write('\n'),
-            call.write('Robert Kupfner'), call.write('\n'),
-	    call.write('Dominick Licciardi'), call.write('\n'),
-            call.write('Ezequiel Lopez'), call.write('\n'),
-            call.write('Chris Johnson'), call.write('\n'),
-            call.write('Gil Leibovich'), call.write('\n')]
+            call.write('Marc Inouye - Dev Team'), call.write('\n'),
+            call.write('Zack Khalidov - Dev Team'), call.write('\n'),
+            call.write('Robert Kupfner - Dev Team'), call.write('\n'),
+	    call.write('Dominick Licciardi - SCRUM Master'), call.write('\n'),
+            call.write('Ezequiel Lopez - Dev Team'), call.write('\n'),
+            call.write('Chris Johnson - Team Owner'), call.write('\n'),
+            call.write('Gil Leibovich - Dev Team'), call.write('\n')]
         )
