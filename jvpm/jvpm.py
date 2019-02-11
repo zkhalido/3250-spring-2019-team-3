fbfa7ff460bfce552b63d0aca469c229e4522154
azuesflip <azuesflip@gmail.com>
2019-02-11 16:55:56 -0700

    Added in bitstring module to jvpm file and am able to pull out the magic part of the header

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -1,4 +1,14 @@
+from collections import namedtuple
+from bitstring import ConstBitStream
+
+
+HeaderInformation = namedtuple("HeaderInformation", "magic minor_version major_version")
+
 def PullHeader():
+	x = ConstBitStream(filename='test.class')
+	b = x.read(32).hex
+	print(b)
+
 	
 
 if '__main__' == __name__:
