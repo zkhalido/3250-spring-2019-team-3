18e4e5ab9982533a07f947e0d520c78f0ffef316
gil <gleibovi@msudenver.edu>
2019-02-13 17:02:57 -0700

    changed to PEP 8, tabs -> spaces; camelCase -> snake_case

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -5,39 +5,39 @@ from bitstring import ConstBitStream
 HeaderInformation = namedtuple("HeaderInformation", "magic minor_version major_version")
 
 def PullHeader():
-	x = ConstBitStream(filename='test.class')
-	headerMagic = x.read(32).hex
-	headerMinor = x.read(16).hex
-	headerMajor = x.read(16).hex
-	headerConstPoolCount = x.read(16).hex
-	headerCPInfo = [None] * (int(headerConstPoolCount, 16) - 1)
-	headerAccessFlag = x.read(16).hex
-	headerThisClass = x.read(16).hex
-	headerSuperClass = x.read(16).hex
-	headerInterfacesCount = x.read(16).hex
-	headerInterfaces = [None] * int(headerInterfacesCount, 16)
-	headerFieldsCount = x.read(16).hex
-	headerFields = [None] * int(headerFieldsCount, 16)
-	headerMethodsCount = x.read(16).hex
-	headerMethods = [None] * int(headerMethodsCount, 16)
-	headerAttributesCount = x.read(16).hex
-	headerAttributes = [None] * int(headerAttributesCount, 16)
-	print(headerMagic)
-	print(headerMinor)
-	print(headerMajor)
-	print(headerConstPoolCount)
-	print(headerCPInfo)
-	print(headerAccessFlag)
-	print(headerThisClass)
-	print(headerSuperClass)
-	print(headerInterfacesCount)
-	print(headerInterfaces)
-	print(headerFieldsCount)
-#	print(headerFields)
-#	print(headerMethodsCount)
-#	print(headerMethods)
-#	print(headerAttributesCount)
-#	print(headerAttributes)
+    x = ConstBitStream(filename='test.class')
+    header_magic = x.read(32).hex
+    header_minor = x.read(16).hex
+    header_major = x.read(16).hex
+    header_const_pool_count = x.read(16).hex
+    headerCPInfo = (int(header_const_pool_count, 16) - 1)
+    header_access_flag = x.read(16).hex
+    header_this_class = x.read(16).hex
+    header_super_class = x.read(16).hex
+    header_interfaces_count = x.read(16).hex
+    header_interfaces = [None] * int(header_interfaces_count, 16)
+    header_fields_count = x.read(16).hex
+    header_fields = [None] * int(header_fields_count, 16)
+    header_methods_count = x.read(16).hex
+    header_methods = [None] * int(header_methods_count, 16)
+    header_attributes_count = x.read(16).hex
+    header_attributes = [None] * int(header_attributes_count, 16)
+    print(header_magic)
+    print(header_minor)
+    print(header_major)
+    print(header_const_pool_count)
+    print(headerCPInfo)
+    print(header_access_flag)
+    print(header_this_class)
+    print(header_super_class)
+    print(header_interfaces_count)
+    print(header_interfaces)
+    print(header_fields_count)
+    # print(header_fields)
+    print(header_methods_count)
+    print(header_methods)
+    # print(header_attributes_count)
+    print(header_attributes)
 
 	
 
