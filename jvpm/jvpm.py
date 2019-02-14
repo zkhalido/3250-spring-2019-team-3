26a8496ac371749561b25209ab912abe035d8ce9
gil <gleibovi@msudenver.edu>
2019-02-13 17:31:18 -0700

    fixed error, accidentally used pylint instead of pylint3- works again

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -4,13 +4,13 @@ from bitstring import ConstBitStream
 
 HeaderInformation = namedtuple("HeaderInformation", "magic minor_version major_version")
 
-def PullHeader():
+def pull_header():
     x = ConstBitStream(filename='test.class')
     header_magic = x.read(32).hex
     header_minor = x.read(16).hex
     header_major = x.read(16).hex
     header_const_pool_count = x.read(16).hex
-    headerCPInfo = (int(header_const_pool_count, 16)-1)
+    header_cp_info = (int(header_const_pool_count, 16)-1)
     header_access_flag = x.read(16).hex
     header_this_class = x.read(16).hex
     header_super_class = x.read(16).hex
@@ -22,24 +22,24 @@ def PullHeader():
     header_methods = [None] * int(header_methods_count, 16)
     header_attributes_count = x.read(16).hex
     header_attributes = [None] * int(header_attributes_count, 16)
-    print header_magic
-    print header_minor
-    print header_major
-    print header_const_pool_count
-    print headerCPInfo
-    print header_access_flag
-    print header_this_class
-    print header_super_class
-    print header_interfaces_count
-    print header_interfaces
-    print header_fields_count
-    # print header_fields
-    print header_methods_count
-    print header_methods
-    # print header_attributes_count
-    print header_attributes
+    print(header_magic)
+    print(header_minor)
+    print(header_major)
+    print(header_const_pool_count)
+    print(header_cp_info)
+    print(header_access_flag)
+    print(header_this_class)
+    print(header_super_class)
+    print(header_interfaces_count)
+    print(header_interfaces)
+    print(header_fields_count)
+    # print(header_fields)
+    print(header_methods_count)
+    # print(header_methods)
+    print(header_attributes_count)
+    print(header_attributes)
 
 	
 
 if '__main__' == __name__:
-    PullHeader()
+    pull_header()
