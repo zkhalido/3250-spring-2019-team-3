2c18ef7fac0873caa1cdc9b2cd9f4591fc409dcc
gil <gleibovi@msudenver.edu>
2019-02-18 19:31:32 -0700

    Bob's code from other repo

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -1,44 +1,55 @@
 from collections import namedtuple
 from bitstring import ConstBitStream
 
+class HeaderClass():
+	def __init__(self):
+            self.x = ConstBitStream(filename='test.class')
+	def PullMagic(self):
+		headerMagic = self.x.read(32).hex
+		print("header: ", headerMagic)
+		return headerMagic
 
-HeaderInformation = namedtuple("HeaderInformation", "magic minor_version major_version")
+	def PullMinor(self):
+		headerMinor = self.x.read(8).uint + self.x.read(8).uint
+		print("minor: ", headerMinor)
+		return headerMinor
 
-def pull_header():
-    x = ConstBitStream(filename='test.class')
-    header_magic = x.read(32).hex
-    header_minor = x.read(16).hex
-    header_major = x.read(16).hex
-    header_const_pool_count = x.read(16).hex
-    header_cp_info = (int(header_const_pool_count, 16)-1)
-    header_access_flag = x.read(16).hex
-    header_this_class = x.read(16).hex
-    header_super_class = x.read(16).hex
-    header_interfaces_count = x.read(16).hex
-    header_interfaces = [None] * int(header_interfaces_count, 16)
-    header_fields_count = x.read(16).hex
-    # header_fields = [None] * int(header_fields_count, 16)
-    header_methods_count = x.read(16).hex
-    # header_methods = [None] * int(header_methods_count, 16)
-    header_attributes_count = x.read(16).hex
-    header_attributes = [None] * int(header_attributes_count, 16)
-    print(header_magic)
-    print(header_minor)
-    print(header_major)
-    print(header_const_pool_count)
-    print(header_cp_info)
-    print(header_access_flag)
-    print(header_this_class)
-    print(header_super_class)
-    print(header_interfaces_count)
-    print(header_interfaces)
-    print(header_fields_count)
-    # print(header_fields)
-    print(header_methods_count)
-    # print(header_methods)
-    print(header_attributes_count)
-    print(header_attributes)
-
+	"""	
+		headerMinor = self.x.read(8).uint + self.x.read(8).uint
+		headerMajor = self.x.read(8).uint + self.x.read(8).uint
+		headerConstPoolCount = self.x.read(8).uint + self.x.read(8).uint
+		headerCPInfo = [None] * (headerConstPoolCount - 1)
+		headerAccessFlag = self.x.read(8).uint + self.x.read(8).uint
+		headerThisClass = self.x.read(8).uint + self.x.read(8).uint
+		headerSuperClass = self.x.read(8).uint + self.x.read(8).uint
+		headerInterfacesCount = self.x.read(8).uint + self.x.read(8).uint
+		headerInterfaces = [None] * headerInterfacesCount
+		headerFieldsCount = self.x.read(8).uint + self.x.read(8).uint
+		headerFields = [None] * headerFieldsCount
+		headerMethodsCount = self.x.read(8).uint + self.x.read(8).uint
+		headerMethods = [None] * headerMethodsCount
+		headerAttributesCount = self.x.read(8).uint + self.x.read(8).uint
+		headerAttributes = [None] * headerAttributesCount
+		print("header: ", headerMagic)
+		print(headerMinor)
+		print(headerMajor)
+		print(headerConstPoolCount)
+		print(len(headerCPInfo))
+		print(headerAccessFlag)
+		print(headerThisClass)
+		print(headerSuperClass)
+		print(headerInterfacesCount)
+		print(len(headerInterfaces))
+		print(headerFieldsCount)
+		print(len(headerFields))
+		print(headerMethodsCount)
+		print(len(headerMethods))
+		print(headerAttributesCount)
+		print(len(headerAttributes))
+	"""
+	
 
 if '__main__' == __name__:
-    pull_header()
+	d = HeaderClass() 
+	d.PullMagic()
+	d.PullMinor()
