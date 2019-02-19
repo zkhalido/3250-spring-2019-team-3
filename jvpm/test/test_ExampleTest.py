64cda593c3072df00e048e884a20915be402d013
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-18 17:40:40 -0700

    test file

--- jvpm/test/test_ExampleTest.py
+++ jvpm/test/test_ExampleTest.py
@@ -10,13 +10,21 @@ class ClassFile():
         magic = ""
         for i in range(4):
             magic += format(self.data[i], '02X')
+        print('hexprint ' + hex(self.data[0]))
+        print('magic ' + magic)
         return magic
 
     def get_minor(self):
+        print("minor", self.data[4], self.data[5])
         return self.data[4] + self.data[5]
 
     def get_major(self):
+        print("major", self.data[6], self.data[7])
         return self.data[6] + self.data[7]
+
+    def get_poolCount(self):
+        print("count", self.data[8], self.data[9])
+        return self.data[8]+ self.data[9]
 
 class OpCodes():
     def __init__(self):
@@ -30,23 +38,28 @@ class OpCodes():
 
 class TestClassFile(unittest.TestCase):
     def setUp(self):
-        m = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x01\x02\x03')
+        m = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x00\x00\x37\x00\x0F')
         with patch(__name__ + '.open', m):
             self.cf = ClassFile()
 
     def test_magic(self):
         self.assertEqual(self.cf.get_magic(), 'CAFEBABE')
+        print("unittestmagic", self.cf.get_magic())
 
     def test_minor(self):
-        self.assertEqual(self.cf.get_minor(), 1)
+        self.assertEqual(self.cf.get_minor(), 0)
+        print("unittestprintminor", self.cf.get_minor())
 
     def test_major(self):
-        self.assertEqual(self.cf.get_major(), 5)
+        self.assertEqual(self.cf.get_major(), 55)
+        print("unittestprintmajor", self.cf.get_major())
+
+    def test_PoolCount(self):
+        self.assertEqual(self.cf.get_poolCount(), 15)
+        print("unittestprintpool", self.cf.get_poolCount())
 
 class TestOpCodes(unittest.TestCase):
     def test_not_implmented(self):
         self.assertEqual(OpCodes().interpret(0), 'not implemented')
         with self.assertRaises(KeyError):
             OpCodes().interpret(1)
-
-
