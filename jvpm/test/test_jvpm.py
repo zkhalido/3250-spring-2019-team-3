3c5a40ae9a9552f2f77b62d1bb4913b90ce2f304
zkhalido <zkhalido@msudenver.edu>
2019-02-18 21:56:38 -0700

    adding unittests to jvpm methods.

--- jvpm/test/test_jvpm.py
+++ jvpm/test/test_jvpm.py
@@ -1,12 +1,25 @@
 import unittest
-import sys
-import jvpm.jvpm
-from unittest.mock import Mock, call
 
-class TestJvpm(unittest.TestCase):
-    def test_jvpm(self):
-        sys.stdout = unittest.mock.Mock()
-        jvpm.jvpm.HeaderClass.PullMagic()
-        sys.stdout.assert_has_calls(
-            [call.write('cafebabe')]
-        )
+#tests jvpm file method outputs
+class TestClassFile(unittest.TestCase):
+    def setUp(self):
+        self.cf = HeaderClass()
+
+    def test_magic(self):
+        self.assertEqual(self.cf.PullMagic(), 'cafebabe')
+        print("unittestmagic: ", self.cf.PullMagic())
+
+    def test_minor(self):
+        self.assertEqual(self.cf.PullMinor(), 0)
+        print("unittestprintminor :", self.cf.PullMinor())
+
+    def test_major(self):
+        self.assertEqual(self.cf.PullMajor(), 54)
+        print("unittestprintmajor: ", self.cf.PullMajor())
+
+    def test_PoolCount(self):
+        self.assertEqual(self.cf.PullConstPoolCount(), 15)
+        print("unittestprintpool: ", self.cf.PullConstPoolCount())
+
+if __name__ == '__main__':
+    unittest.main()
