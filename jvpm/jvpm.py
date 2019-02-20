c8f5e826fb7e624623af1dea92acc7db1ba09e6e
Bob Kupfner <azuesflip@gmail.com>
2019-02-20 00:56:58 -0700

    added into classes

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -71,6 +71,42 @@ class OpCodes():
     def interpret(self, value):
         return self.table[value]()
 
+class UnittestHeader(unittest.TestCase):
+	def setUp(self):
+		m = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x00\x00\x36\x00\x0F')
+		with patch(__name__ + '.open', m):
+			self.cf = HeaderClass()
+
+	def test_magic(self):
+		self.assertEqual(self.cf.get_magic(), 'CAFEBABE')
+		print("unittestmagic", self.cf.get_magic())
+
+	def test_minor(self):
+		self.assertEqual(self.cf.get_minor(), 0)
+		print("unittestprintminor", self.cf.get_minor())
+
+	def test_major(self):
+		self.assertEqual(self.cf.get_major(), 54)
+		print("unittestprintmajor", self.cf.get_major())
+
+	def test_PoolCount(self):
+		self.assertEqual(self.cf.get_poolCount(), 15)
+		print("unittestprintpool", self.cf.get_poolCount())
+
+class TestOpCodes(unittest.TestCase):
+	def setUp(self):
+		m = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x00\x00\x36\x00\x0F')
+		with patch(__name__ + '.open', m):
+			self.cf = OpCodes()
+
+	def test_interpret(self, value):
+		self.assertEqual(self.cf.interpret(1), 'aconst_null')
+
+	def test_not_implmented(self):
+		self.assertEqual(OpCodes().interpret(1), 'aconst_null')
+		with self.assertRaises(KeyError):
+			OpCodes().interpret(1)
+
 if '__main__' == __name__:
 	d = HeaderClass()
 	d.PullMagic()
