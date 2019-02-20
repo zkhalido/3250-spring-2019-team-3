f0f9d5309096ca326aca5c9ed7badf9c14e10b67
Bob Kupfner <azuesflip@gmail.com>
2019-02-20 00:58:51 -0700

    tests are working other than opcodes which is to be expected

--- jvpm/test/test_jvpm.py
+++ jvpm/test/test_jvpm.py
@@ -1,40 +1,22 @@
 import unittest
 import sys
 import jvpm
-from unittest.mock import Mock, call
+from unittest.mock import Mock, call, patch, mock_open
 
-class TestClassFile(unittest.TestCase):
+class UnittestHeader(unittest.TestCase):
 	def setUp(self):
-		m = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x00\x00\x36\x00\x0F')
+		m = mock_open(read_data='CAFEBABE00000036000F')
 		with patch(__name__ + '.open', m):
-			self.cf = ClassFile()
+			self.cf = jvpm.HeaderClass()
 
 	def test_magic(self):
-		self.assertEqual(self.cf.get_magic(), 'CAFEBABE')
-		print("unittestmagic", self.cf.get_magic())
-
-	def test_minor(self):
-		self.assertEqual(self.cf.get_minor(), 0)
-		print("unittestprintminor", self.cf.get_minor())
-
-	def test_major(self):
-		self.assertEqual(self.cf.get_major(), 54)
-		print("unittestprintmajor", self.cf.get_major())
-
-	def test_PoolCount(self):
-		self.assertEqual(self.cf.get_poolCount(), 15)
-		print("unittestprintpool", self.cf.get_poolCount())
+		self.assertEqual(self.cf.PullMagic(), 'cafebabe')
+		self.assertEqual(self.cf.PullMinor(), 0)
+		self.assertEqual(self.cf.PullMajor(), 54)
+		self.assertEqual(self.cf.PullConstPoolCount(), 15)
 
 class TestOpCodes(unittest.TestCase):
-	def setUp(self):
-		m = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x00\x00\x36\x00\x0F')
-		with patch(__name__ + '.open', m):
-			self.cf = OpCodes()
-
-	def test_interpret(self, value):
-		self.assertEqual(self.cf.interpret(1), 'aconst_null')
-
 	def test_not_implmented(self):
-		self.assertEqual(OpCodes().interpret(1), 'aconst_null')
+		self.assertEqual(jvpm.OpCodes().interpret(0), 'not implemented')
 		with self.assertRaises(KeyError):
-			OpCodes().interpret(1)
+			jvpm.OpCodes().interpret(1)
