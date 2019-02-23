5465e00c5eb7672c98d9da094082074f954d676d
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-23 14:50:15 -0700

    Update jvpm_methods.py

--- jvpm/jvpm_methods.py
+++ jvpm/jvpm_methods.py
@@ -1,11 +1,11 @@
 
 def function1():
-    print "called function 1"
+    print("called function 1")
 
 def function2():
-    print "called function 2"
+    print("called function 2")
 
 def function3():
-    print "called function 3"
+    print("called function 3")
 
 tokenDict = {"iconst_m1":function1, "istore_1":function2, "iinc":function3}
