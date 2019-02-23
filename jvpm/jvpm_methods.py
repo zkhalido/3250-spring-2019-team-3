e9bf20332d2ec944c3f9bc1752396bdfd52c5515
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-23 15:33:59 -0700

    Update jvpm_methods.py

--- jvpm/jvpm_methods.py
+++ jvpm/jvpm_methods.py
@@ -9,3 +9,7 @@ def method3():
     print("called function 3")
 
 tokenDict = {"iconst_m1":method1, "istore_1":method2, "iinc":method3}
+
+def get_methods(opcode):
+    ''' Retrieve byte code from dictionary and return byte code '''
+    return tokenDict[opcode]
