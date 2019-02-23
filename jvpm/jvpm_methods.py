82ee19c00a3ac3a80b402a46406a590446d376d1
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-23 15:00:46 -0700

    Update jvpm_methods.py

--- jvpm/jvpm_methods.py
+++ jvpm/jvpm_methods.py
@@ -1,11 +1,11 @@
 
-def function1():
+def method1():
     print("called function 1")
 
-def function2():
+def method2():
     print("called function 2")
 
-def function3():
+def method3():
     print("called function 3")
 
-tokenDict = {"02":function1, "3c":function2, "84":function3}
+tokenDict = {"02":method1, "3c":method2, "84":method3}
