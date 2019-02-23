8b8775064bf0b431bd6570f587000f1828fb7ac0
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-23 13:42:19 -0700

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -271,3 +271,4 @@ if '__main__' == __name__:
         elif OP_VALUE_1 == 10:
             VARIABLE_3 = VARIABLE_4
             print('      <<<< VARIABLE_3++ = ' + str(VARIABLE_3) + ' >>>>')
+        print()
