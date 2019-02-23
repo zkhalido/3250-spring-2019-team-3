6baa2a49f2e8cdf9454fa944abc7f34a8c37a97f
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-23 03:52:33 -0700

    Update jvpm_stack.py

--- jvpm/jvpm_stack.py
+++ jvpm/jvpm_stack.py
@@ -1,26 +1,25 @@
-# class Stack:
-#     """Class that creates a Stack, with methods to interact with the Stack"""
-def __init__(self):
-    self.items = []
-    self.variable_1 = ""
+class Stack:
+    """Class that creates a Stack, with methods to interact with the Stack"""
+    def __init__(self):
+        self.items = []
 
-def is_empty(self):
-    """test if Stack is empty"""
-    return self.items == []
+    def is_empty(self):
+        """test if Stack is empty"""
+        return self.items == []
 
-def push(self, item):
-    """Put item on the stack"""
-    self.items.append(item)
+    def push(self, item):
+        """Put item on the stack"""
+        self.items.append(item)
 
-def pop(self):
-    """Take item off stack"""
-    return self.items.pop()
+    def pop(self):
+        """Take item off stack"""
+        return self.items.pop()
 
-def peek(self):
-    """Look at item on top of stack"""
-    return self.items[len(self.items) - 1]
+    def peek(self):
+        """Look at item on top of stack"""
+        return self.items[len(self.items)-1]
 
-def size(self):
-    """Tells how many items are on the stack"""
-    return len(self.items)
+    def size(self):
+        """Tells how many items are on the stack"""
+        return len(self.items)
         
