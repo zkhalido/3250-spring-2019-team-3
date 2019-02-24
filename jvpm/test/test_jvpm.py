e0f25726b636108c10a287fc83720653589797e8
chris <cjohn268@msudenver.edu>
2019-02-24 12:41:56 -0700

    just fixing some stuff in test_jvpm, will add the rest of the tests there

--- jvpm/test/test_jvpm.py
+++ jvpm/test/test_jvpm.py
@@ -1,18 +1,20 @@
 import sys
-import jvpm
+import unittest
+
+import jvpm_opcodes
 from unittest.mock import Mock, call, patch, mock_open
 
 class UnittestHeader(unittest.TestCase):
 	def setUp(self):
 		m = mock_open(read_data='CAFEBABE00000036000F')
 		with patch(__name__ + '.open', m):
-			self.cf = jvpm.HeaderClass()
+			self.cf = jvpm_opcodes.HeaderClass()
 
 	def test_magic(self):
-		self.assertEqual(self.cf.PullMagic(), 'cafebabe')
-		self.assertEqual(self.cf.PullMinor(), 0)
-		self.assertEqual(self.cf.PullMajor(), 54)
-		self.assertEqual(self.cf.PullConstPoolCount(), 15)
+		self.assertEqual(self.cf.pull_magic(), 'cafebabe')
+		self.assertEqual(self.cf.pull_minor(), 0)
+		self.assertEqual(self.cf.pull_major(), 54)
+		self.assertEqual(self.cf.pull_const_pool_count(), 14)
 
 class TestOpCodes(unittest.TestCase):
 	def test_not_implmented(self):
