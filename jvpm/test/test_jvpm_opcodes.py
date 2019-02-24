eb3353c97d7bb87a0908ef3243f02ca603db119e
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-24 02:47:29 -0700

    Update test_jvpm_opcodes.py

--- jvpm/test/test_jvpm_opcodes.py
+++ jvpm/test/test_jvpm_opcodes.py
@@ -1,4 +1,4 @@
-import jvpm_opcodes
+import jvpm.jvpm_opcodes
 # Unittest to test the output of the HeaderClass() methods.
 # python3 -m unittest jvpm_opcodes.py
 # We have a warning about an unclosed file but no errors.
