f651cc984843b9b26de3834572723a2544b66f0a
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-25 16:27:31 -0700

    Update test1.java

--- jvpm/test1.java
+++ jvpm/test1.java
@@ -1,6 +1,1 @@
-class test { static void main(String[] args) { 
-  int a = 1; 
-  int b = 2; 
-  int c = a + b; 
-  } 
-}
+class test { static void main(String[] args) { int a = 1; int b = 2; int c = a + b; }; }
