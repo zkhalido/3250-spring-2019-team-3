5c02dd2b4a6b80f48632b205590c1c898512f7c9
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-25 17:29:24 -0700

    added test for math commands

--- jvpm/test1.java
+++ jvpm/test1.java
@@ -1,1 +1,10 @@
-class test1 {static void main(String[] args) {int a = 1; int b = 2; int c = a + b; }; }
+class test1 {
+    static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        int d = a - b;
+        int e = b / a;
+        int f = a * b;
+    };
+}
