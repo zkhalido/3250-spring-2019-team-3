f008e264503d36bf9ff175182ae1c3a77ca0af3f
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-25 18:49:45 -0700

    Update test1.java

--- jvpm/test1.java
+++ jvpm/test1.java
@@ -4,9 +4,9 @@ class test1 {
         int a = 1;
         int b = 2;
         int c = a + b;
-        int d = a - b;
-        int e = b / a;
-        int f = a * b;
+//         int d = a - b;
+//         int e = b / a;
+//         int f = a * b;
     };
 }
 =======
