99cb2400b396837356268a8e1bab1a008c1b5aa0
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-26 23:12:28 -0700

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -93,8 +93,8 @@ class OpCodes():
         print()
         index = 0
         while index < len(self.opcodes):
-            Opcode_to_call_[index] = jvpm_dict.get_opcode(self.opcodes[index])
-            print("\nOpcode to implement from bytecode " + self.opcodes[index] + ': ' + Opcode_to_call_[index])
+            opcall[index] = jvpm_dict.get_opcode(self.opcodes[index])
+            print("\nOpcode to implement from bytecode " + self.opcodes[index] + ': ' + opcall[index])
             index += 1
         print()
 
