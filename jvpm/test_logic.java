f7a07466faefc1af2e5151b16135968d68bb7171
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-27 16:14:27 -0700

    Update test_logic.java

--- jvpm/test_logic.java
+++ jvpm/test_logic.java
@@ -1,8 +1,7 @@
 class test_logic {
     static void main(String[] args) {
-//         int a = 1;
-//         int b = 2;
-//         int c = a & b;
-        print(5 + 1);
+        int a = 1;
+        int b = 2;
+        int c = a & b;
     };
 }
