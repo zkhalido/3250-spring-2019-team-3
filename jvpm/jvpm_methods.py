7cb56cd3c8d5ee55f8e33dbcd54a468445cbba2d
minouye2 <minouye2@msudenver.edu>
2019-02-27 16:23:00 -0700

    added parenthesis to iushr

--- jvpm/jvpm_methods.py
+++ jvpm/jvpm_methods.py
@@ -393,10 +393,10 @@ def iushr():
     """int logical shift right"""
     var2 = S.pop()
     var1 = S.pop()
-    if val1 >= 0
+    if (val1 >= 0)
         push(val1 >> val2)
     else
-        push((val1 + 0x10000000) >> val2)
+        push(val1 + 0x10000000) >> val2)
     print(S.peek())
 
 def ixor():
