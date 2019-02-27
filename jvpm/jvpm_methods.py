1151e984e503cae9c8c9c9386148f1d96aeec3f0
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-27 16:28:35 -0700

    Update jvpm_methods.py

--- jvpm/jvpm_methods.py
+++ jvpm/jvpm_methods.py
@@ -394,9 +394,9 @@ def iushr():
     var2 = S.pop()
     var1 = S.pop()
     if var1 >= 0:
-        push(var1 >> var2)
+        S.push(var1 >> var2)
     else:
-        push((var1 + 0x10000000) >> var2)
+        S.push((var1 + 0x10000000) >> var2)
     print(S.peek())
 
 def ixor():
