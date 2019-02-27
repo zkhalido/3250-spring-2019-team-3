41bfd44164af3adbdaad726d6b24bfe22896090d
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-27 14:57:08 -0700

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -49,8 +49,9 @@ class OpCodes():
     opcodes, and implement the methods using the external dictionary of methods"""
     def __init__(self):
         # List of the test1.java(math) opcodes.
-        self.opcodes = ['04', '3c', '05', '3d', '1b', '1c', '60', '1c', '68', '1c', '6c', '1c',
-                        '64', '3e']
+#         self.opcodes = ['04', '3c', '05', '3d', '1b', '1c', '60', '1c', '68', '1c', '6c', '1c',
+#                         '64', '3e']
+	self.opcodes = ['04', '3c', '05', '3d', '1b', '1c', '7e', '3e']
 
         """
 
