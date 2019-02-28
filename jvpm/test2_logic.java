95f74143e02a0c3bf62106f916436ea12c3b2d75
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-27 20:41:50 -0700

    Update test2_logic.java

--- jvpm/test2_logic.java
+++ jvpm/test2_logic.java
@@ -3,5 +3,22 @@ class test2_logic {
         int a = 1;
         int b = 2;
         int c = ((((((a & b) >> b) << a) | a) % b) ^ a);
+        
+//      int a = 5;               // value1
+// 		int b = 1;               // value2
+		
+// 		boolean ineg = !(a < b); // negate int, 
+		
+// 		int ior = ( a|b );       // bitwise int OR	
+		
+// 		int irem = a % b ;       // logical int remainder
+		
+// 		int ishl = a << b;       // shift left		      
+		
+// 		int ishr = a >> b;       // int arithmetic shift right
+		
+// 		int iushr = a >>> b;     // int logical shift right 
+		
+// 		int ixor = a ^ b ;       // int xor           
     };
 }
