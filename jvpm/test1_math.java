fa624bbe4a9f403ba1096db95850afa272ac9bd6
Dominick Licciardi <dominick.nola@gmail.com>
2019-02-27 20:57:47 -0700

    Update test1_math.java

--- jvpm/test1_math.java
+++ jvpm/test1_math.java
@@ -2,6 +2,7 @@ class test1_math {
     static void main(String[] args) {
         int a = 1;
         int b = 2;
-        int c = ((((a + b) * b) / b) - b);
+        int c = a + b;
+        // int c = ((((a + b) * b) / b) - b);
     };
 }
