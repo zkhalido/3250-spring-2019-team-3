43e137773844f4396242867f3f7a90ea960f8ca9
zkhalido <zkhalido@msudenver.edu>
2019-02-28 21:06:25 -0700

    Method tests.

--- jvpm/test/test_jvpm_methods.py
+++ jvpm/test/test_jvpm_methods.py
@@ -1,7 +1,11 @@
 import unittest
-import jvpm_methods
 
 class TestOpcodeMethods(unittest.TestCase):
+    def setUp(self):
+        self.test = jvpm_methods()
 
     def test_iadd(self):
-        
+        self.assertEqual()
+
+if __name__ == '__main__':
+    unittest.main()
