51280ba1c3d327de4614c5e50067a9e31902c049
Dominick Licciardi <dominick.nola@gmail.com>
2019-03-01 09:44:08 -0700

    Update jvpm.py

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -66,7 +66,7 @@ class HeaderClass():
     def get_interfaces_count(self):
         holder = self.get_const_pool_count() + 9
         joined = self.data[holder + 6]
-        print("test " + joined)
+        print("test " + str(joined))
         print("Interfaces Count: ", self.data[holder + 6] + self.data[holder + 7])
         print(format(self.data[holder + 6], '02X'))
         print(format(self.data[holder + 7], '02X'))
