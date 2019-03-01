f14b433ac099b6e88f4eb029d450fd9aae235283
zkhalido <zkhalido@msudenver.edu>
2019-02-28 22:14:52 -0700

    new tests.

--- jvpm/test/test_stack.py
+++ jvpm/test/test_stack.py
@@ -9,5 +9,33 @@ class TestStack(unittest.TestCase):
     def test_is_empty(self):
         S = stack.Stack()
         S.push(3)
+        self.assertFalse(S.is_empty())
         S.pop()
         self.assertTrue(S.is_empty())
+
+    def test_push(self):
+        S = stack.Stack()
+        S.push(2)
+        S.push(3)
+        v = S.pop()
+        self.assertEqual(v, 3)
+
+    def test_pop(self):
+        S = stack.Stack()
+        S.push(3)
+        S.push(2)
+        S.push(4)
+        S.push(0)
+        a = S.pop()
+        b = S.pop()
+
+        self.assertEqual(a, 0)
+        self.assertEqual(b, 4)
+
+    def test_peek(self):
+        S = stack.Stack()
+        S.push("hello")
+        S.push("hi")
+        self.assertEqual(S.peek(),"hi")
+        S.pop()
+        
