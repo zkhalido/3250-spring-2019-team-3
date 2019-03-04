b29e374374132a065d687ee3023db153c00b4cea
zkhalido <zkhalido@msudenver.edu>
2019-03-04 14:43:57 -0700

    .

--- jvpm/stack.py
+++ jvpm/stack.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Stack:
     """Class that creates a Stack, with methods to interact with the Stack"""
     def __init__(self):
@@ -22,29 +21,4 @@ class Stack:
 
     def size(self):
         """Tells how many items are on the stack"""
-=======
-class Stack:
-    """Class that creates a Stack, with methods to interact with the Stack"""
-    def __init__(self):
-        self.items = []
-
-    def is_empty(self):
-        """test if Stack is empty"""
-        return self.items == []
-
-    def push(self, item):
-        """Put item on the stack"""
-        self.items.append(item)
-
-    def pop(self):
-        """Take item off stack"""
-        return self.items.pop()
-
-    def peek(self):
-        """Look at item on top of stack"""
-        return self.items[len(self.items)-1]
-
-    def size(self):
-        """Tells how many items are on the stack"""
->>>>>>> testing_travis_testing_stack
         return len(self.items)
