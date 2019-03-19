596f426bf192a961cda99b997c54c8cfdfce247c
Chris Johnson <43618289+Cjohnson187@users.noreply.github.com>
2019-03-19 09:51:02 -0600

    imported new module in test

--- jvpm/test/test_jvpm.py
+++ jvpm/test/test_jvpm.py
@@ -4,6 +4,7 @@ from unittest.mock import mock_open, patch, Mock, call
 from jvpm.stack import Stack
 from jvpm.jvpm_methods import OpCodeMethods
 from jvpm.jvpm_opcodes import OpCodes
+from jvpm.pool_translate import PoolTranslate
 
 import jvpm.jvpm_dict
 import jvpm.jvpm_opcodes
