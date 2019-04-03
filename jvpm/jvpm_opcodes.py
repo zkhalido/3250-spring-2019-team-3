679d16b695d9a7e76a9dc2dfb1c25d25aedf8bd4
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 04:53:21 -0600

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -160,18 +160,18 @@ class OpCodes():
 
         """
         
-      def dict_search(self):
-        print(self.opcodes)
-        index = 0
+     def dict_search(self):
+       print(self.opcodes)
+       index = 0
 
-        while index < len(self.opcodes):
-            opcall = jvpm_dict.get_opcode(self.opcodes[index])
+       while index < len(self.opcodes):
+           opcall = jvpm_dict.get_opcode(self.opcodes[index])
 
-            print (opcall) # just to see what opcall is passed through
-            jvpm_methods.opcode_methods(opcall)
-            # jvMethodsIn.token_dict(opcall)
-            index += 1
-        print()  
+           print (opcall) # just to see what opcall is passed through
+           jvpm_methods.opcode_methods(opcall)
+           # jvMethodsIn.token_dict(opcall)
+           index += 1
+       print()  
 
 #     def dict_search(self, jvMethodsIn):
 #         print(self.opcodes)
