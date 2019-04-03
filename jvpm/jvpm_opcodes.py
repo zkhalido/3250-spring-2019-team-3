62ba3f074798564ff2ae9f5c652e944c69585353
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 04:55:35 -0600

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -4,7 +4,7 @@ from pip._vendor.distlib.compat import raw_input
 
 import jvpm_dict    # import external opcode dictionary
 from collections import defaultdict
-#import jvpm_methods
+import jvpm_methods
 #from jvpm_methods import OpCodeMethods # import external method dictionary
 
 # **************************************************************************************************
