3d38440a5af8d0a79489f5da01e1a8bfaca2536e
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 06:08:04 -0600

    Update jvpm_opcodes.py

--- jvpm/jvpm_opcodes.py
+++ jvpm/jvpm_opcodes.py
@@ -204,12 +204,15 @@ if '__main__' == __name__:                  #pragma: no cover
 
     # **********************************************************************************************
 
-    print('\n2) ___Parse, pull, and assign method bytecodes to an array, search imported '      #pragma: no cover
-          '\n  opcode dictionary for bytecode and pull opcode. If found, send opcode to'        #pragma: no cover
-          '\n  imported method dictionary to implement the method:___')                         #pragma: no cover
+    print('\n2) ___Parse, pull, and assign method bytecodes to an array, search imported '      
+          '\n  opcode dictionary for bytecode and pull opcode. If found, send opcode to'        
+          '\n  imported method dictionary to implement the method:___')                         
 
-    O = OpCodes()               #pragma: no cover
+    O = OpCodes()         
     O.dict_search()
-    print(H.get_const_pool())   #pragma: no cover
-    #O.dict_search() will need to pass through the oject that holds all the methods
-    # also need to have actual constant that are held in the object with the opcall methods
+    
+    # **********************************************************************************************
+    
+    print('\n3) ___Retrieve the Constant Pool from the .class file and print to console. ')                       
+
+    print(H.get_const_pool()) 
