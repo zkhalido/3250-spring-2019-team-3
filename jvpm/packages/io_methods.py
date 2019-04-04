043ec5378cc0e72815acf560f558ec38f5160803
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 22:37:45 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -6,7 +6,7 @@ class IoMethods():
     def keyboard_in(self):
         """input from your keyboard"""
         var1 = input("What's your first variable? ")
-        print("Your first variable is: " + var1 + "!")
+        print("Your first variable is: " + var1)
         var2 = input("What's your second variable? ")
-        print("So, you are ready to add " + var1 + " + " + var2 + "!")
+        print("So, you are ready to add " + var1 + " + " + var2)
     
