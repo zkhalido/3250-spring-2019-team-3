67536f98069e579109bcec81976cf952d9e5a35c
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 23:06:17 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -54,7 +54,7 @@ if '__main__' == __name__:                  #pragma: no cover
     print('\n5) ___Take input from the keyboard and add 2 numbers:___\n')
 
     K = packages.io_methods.IoMethods()
-    K.keyboard_in()
+    K.keyboard_add()
 
 
     # **********************************************************************************************
