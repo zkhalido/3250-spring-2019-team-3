ff16d7fc5f0e567a13510bdb2a41f567ee0dea53
Azuesflip <azuesflip@gmail.com>
2019-04-03 21:23:14 -0600

    possible refactoring of the file.

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -35,36 +35,27 @@ class HeaderClass():
 
     def get_const_pool(self):
         temp = defaultdict(list)
-        position = 0
         count = self.get_const_pool_count() - 1
         for i in range(count):
             # Pulling class info
             if self.data[10 + i + position] == 7:
-                temp[i].append(format(self.data[10 + i + position], '02x'))
-                temp[i].append(format(self.data[11 + i + position] + self.data[12 + i + position], '02x'))
+                temp[i] = single_part_tuple(self, 10 + i + position)
                 position += 2
             # Field Ref
             elif self.data[10 + i + position] == 9:
-                temp[i].append(format(self.data[10 + i + position], '02x'))
-                temp[i].append(format(self.data[11 + i + position] + self.data[12 + i + position], '02x'))
-                temp[i].append(format(self.data[13 + i + position] + self.data[14 + i + position], '02x'))
+                temp[i] = double_part_tuple(self, 10 + i + position)
                 position += 4
             # Method Ref
             elif self.data[10 + i + position] == 10:
-                temp[i].append(format(self.data[10 + i + position], '02x'))
-                temp[i].append(format(self.data[11 + i + position] + self.data[12 + i + position], '02x'))
-                temp[i].append(format(self.data[13 + i + position] + self.data[14 + i + position], '02x'))
+                temp[i] = double_part_tuple(self, 10 + i + position)
                 position += 4
             # Interface Method Ref
             elif self.data[10 + i + position] == 11:
-                temp[i].append(format(self.data[10 + i + position], '02x'))
-                temp[i].append(format(self.data[11 + i + position] + self.data[12 + i + position], '02x'))
-                temp[i].append(format(self.data[13 + i + position] + self.data[14 + i + position], '02x'))
+                temp[i] = double_part_tuple(self, 10 + i + position)
                 position += 4
             # String
             elif self.data[10 + i + position] == 8:
-                temp[i].append(format(self.data[10 + i + position], '02x'))
-                temp[i].append(format(self.data[11 + i + position] + self.data[12 + i + position], '02x'))
+                temp[i] = single_part_tuple(self, 10 + i + position)
                 position += 4
             # Integer
             elif self.data[10 + i + position] == 3:
@@ -128,79 +119,31 @@ class HeaderClass():
                 position += 4
         return temp
 
-    def get_const_pool_length(self):
-    #    temp = defaultdict(list)
-        position = 0
-        count = self.get_const_pool_count() - 1
-        for i in range(count):
-            # Pulling class info
-            if self.data[10 + i + position] == 7:
-                position += 2
-            # Field Ref
-            elif self.data[10 + i + position] == 9:
-                position += 4
-            # Method Ref
-            elif self.data[10 + i + position] == 10:
-                position += 4
-            # Interface Method Ref
-            elif self.data[10 + i + position] == 11:
-                position += 4
-            # String
-            elif self.data[10 + i + position] == 8:
-                position += 2
-            # Integer
-            elif self.data[10 + i + position] == 3:
-                position += 4
-            # Float
-            elif self.data[10 + i + position] == 4:
-                position += 4
-            # Long
-            elif self.data[10 + i + position] == 5:
-                position += 8
-            # Double
-            elif self.data[10 + i + position] == 6:
-                position += 8
-            # Name and Type
-            elif self.data[10 + i + position] == 12:
-                position += 4
-            # Utf_8
-            elif self.data[10 + i + position] == 1:
-                position += (self.data[11 + i + position] + self.data[12 + i + position])
-                position += 2
-            # Method Handle
-            elif self.data[10 + i + position] == 15:
-                position += 3
-            # Method Type
-            elif self.data[10 + i + position] == 16:
-                position += 2
-            # Invoke Dynamic
-            elif self.data[10 + i + position] == 18:
-                position += 4
-        return position
+    
 
     def get_access_flags(self):
-        holder = self.get_const_pool_length() + 24
+        holder = position + 24
         #print("Access Flags: ", self.data[holder] + self.data[holder + 1])
         #print(format(self.data[holder], '02X'))
         #print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
 
     def get_this_class(self):
-        holder = self.get_const_pool_length() + 24
+        holder = position + 24
         #print("This Class: ", self.data[holder + 2] + self.data[holder + 3])
         #print(format(self.data[holder + 2], '02X'))
         #print(format(self.data[holder + 3], '02X'))
         return self.data[holder + 2] + self.data[holder + 3]
 
     def get_super_class(self):
-        holder = self.get_const_pool_length() + 24
+        holder = position + 24
         #print("Super Class: ", self.data[holder + 4] + self.data[holder + 5])
         #print(format(self.data[holder + 4], '02X'))
         #print(format(self.data[holder + 5], '02X'))
         return self.data[holder + 4] + self.data[holder + 5]
 
     def get_interfaces_count(self):
-        holder = self.get_const_pool_length() + 25
+        holder = position + 25
         #joined = ''.join([format(self.data[holder + 6], '02X'), format(self.data[holder + 7], '02X')])
         #print(joined)
         #intj = int(joined)
@@ -214,7 +157,7 @@ class HeaderClass():
     def get_interfaces(self):
         temp = []
         count = self.get_interfaces_count()
-        index = self.get_const_pool_length() + 16
+        index = position + 16
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
         #print("Interfaces Length: ", len(temp))
@@ -222,7 +165,7 @@ class HeaderClass():
         return temp
 
     def get_fields_count(self):
-        holder = self.get_const_pool_length() + self.get_interfaces_count() + 33
+        holder = position + self.get_interfaces_count() + 33
         #print(format(self.data[holder], '02X'))
         #print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
@@ -230,7 +173,7 @@ class HeaderClass():
     def get_fields(self):
         temp = []
         count = self.get_fields_count()
-        index = self.get_const_pool_length() + self.get_interfaces_count() + 33
+        index = position + self.get_interfaces_count() + 33
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
         #print("Fields Length: ", len(temp))
@@ -238,7 +181,7 @@ class HeaderClass():
         return temp
 
     def get_methods_count(self):
-        holder = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + 35
+        holder = position + self.get_interfaces_count() + self.get_fields_count() + 35
         #print(format(self.data[holder], '02X'))
         #print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
@@ -246,7 +189,7 @@ class HeaderClass():
     def get_methods(self):
         temp = []
         count = self.get_methods_count()
-        index = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + 35
+        index = position + self.get_interfaces_count() + self.get_fields_count() + 35
         print("index = " + str(index))
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
@@ -256,7 +199,7 @@ class HeaderClass():
         return temp
 
     def get_attributes_count(self):
-        holder = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + self.get_methods_count() + 20
+        holder = position + self.get_interfaces_count() + self.get_fields_count() + self.get_methods_count() + 20
         print(format(self.data[holder], '02X'))
         print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
@@ -264,11 +207,31 @@ class HeaderClass():
     def get_attributes(self):
         temp = []
         count = self.get_attributes_count()
-        index = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + self.get_methods_count() + 22
+        index = position + self.get_interfaces_count() + self.get_fields_count() + self.get_methods_count() + 22
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
         print("Attributes Length: ", len(temp))
         print(temp)
+        return temp
+
+    def single_part_tuple(self, current):
+        idNum = format(self.data[current], '02x')
+        info = format(self.data[current + 1] + self.data[current + 2], '02x')
+        temp = (idNum, info)
+        return temp
+
+    def single_part_long_tuple(self, current):
+        idNum = format(self.data[current], '02x')
+        info = self.data[11 + i + position] + self.data[12 + i + position] +
+                    self.data[13 + i + position] + self.data[14 + i + position], '02x')
+        temp = (idNum, info)
+        return temp
+
+    def double_part_tuple(self, current):
+        idNum = format(self.data[current], '02x')
+        info = format(self.data[current + 1] + self.data[current + 2], '02x')
+        info2 = format(self.data[current + 3] + self.data[current + 4], '02x')
+        temp = (idNum, info, info2)
         return temp
 
 if '__main__' == __name__:
