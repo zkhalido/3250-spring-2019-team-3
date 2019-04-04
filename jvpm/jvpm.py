057d0c1b65b79336612f34cd6feccf0046835293
Azuesflip <azuesflip@gmail.com>
2019-04-03 20:18:08 -0600

    stashing some information

--- jvpm/jvpm.py
+++ jvpm/jvpm.py
@@ -19,15 +19,15 @@ class HeaderClass():
         magic = ""
         for i in range(4):
             magic += format(self.data[i], '02X')
-        print("Magic: ", magic)
+        #print("Magic: ", magic)
         return magic
 
     def get_minor(self):
-        print("Minor: ", self.data[4] + self.data[5])
+        #print("Minor: ", self.data[4] + self.data[5])
         return self.data[4] + self.data[5]
 
     def get_major(self):
-        print("Major: ", self.data[6] + self.data[7])
+        #print("Major: ", self.data[6] + self.data[7])
         return self.data[6] + self.data[7]
 
     def get_const_pool_count(self):
@@ -181,23 +181,23 @@ class HeaderClass():
 
     def get_access_flags(self):
         holder = self.get_const_pool_length() + 24
-        print("Access Flags: ", self.data[holder] + self.data[holder + 1])
-        print(format(self.data[holder], '02X'))
-        print(format(self.data[holder + 1], '02X'))
+        #print("Access Flags: ", self.data[holder] + self.data[holder + 1])
+        #print(format(self.data[holder], '02X'))
+        #print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
         
     def get_this_class(self):
         holder = self.get_const_pool_length() + 24
-        print("This Class: ", self.data[holder + 2] + self.data[holder + 3])
-        print(format(self.data[holder + 2], '02X'))
-        print(format(self.data[holder + 3], '02X'))
+        #print("This Class: ", self.data[holder + 2] + self.data[holder + 3])
+        #print(format(self.data[holder + 2], '02X'))
+        #print(format(self.data[holder + 3], '02X'))
         return self.data[holder + 2] + self.data[holder + 3]
 
     def get_super_class(self):
         holder = self.get_const_pool_length() + 24
-        print("Super Class: ", self.data[holder + 4] + self.data[holder + 5])
-        print(format(self.data[holder + 4], '02X'))
-        print(format(self.data[holder + 5], '02X'))
+        #print("Super Class: ", self.data[holder + 4] + self.data[holder + 5])
+        #print(format(self.data[holder + 4], '02X'))
+        #print(format(self.data[holder + 5], '02X'))
         return self.data[holder + 4] + self.data[holder + 5]
 
     def get_interfaces_count(self):
@@ -218,36 +218,36 @@ class HeaderClass():
         index = self.get_const_pool_length() + 16
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
-        print("Interfaces Length: ", len(temp))
-        print(temp)
+        #print("Interfaces Length: ", len(temp))
+        #print(temp)
         return temp
 
     def get_fields_count(self):
-        holder = self.get_const_pool_length() + self.get_interfaces_count() + 16
-        # print(format(self.data[holder], '02X'))
-        # print(format(self.data[holder + 1], '02X'))
+        holder = self.get_const_pool_length() + self.get_interfaces_count() + 33
+        #print(format(self.data[holder], '02X'))
+        #print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
 
     def get_fields(self):
         temp = []
         count = self.get_fields_count()
-        index = self.get_const_pool_length() + self.get_interfaces_count() + 18
+        index = self.get_const_pool_length() + self.get_interfaces_count() + 33
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
-        print("Fields Length: ", len(temp))
-        print(temp)
+        #print("Fields Length: ", len(temp))
+        #print(temp)
         return temp
 
     def get_methods_count(self):
-        holder = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + 14
-        # print(format(self.data[holder], '02X'))
-        # print(format(self.data[holder + 1], '02X'))
+        holder = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + 35
+        #print(format(self.data[holder], '02X'))
+        #print(format(self.data[holder + 1], '02X'))
         return self.data[holder] + self.data[holder + 1]
 
     def get_methods(self):
         temp = []
         count = self.get_methods_count()
-        index = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + 16
+        index = self.get_const_pool_length() + self.get_interfaces_count() + self.get_fields_count() + 35
         print("index = " + str(index))
         for i in range(count):
             temp.append(format(self.data[index + i], '02X'))
@@ -277,7 +277,7 @@ if '__main__' == __name__:
     d.get_magic()
     d.get_minor()
     d.get_major()
-    print(d.get_const_pool())
+    #print(d.get_const_pool())
     d.get_const_pool()
     d.get_access_flags()
     d.get_this_class()
