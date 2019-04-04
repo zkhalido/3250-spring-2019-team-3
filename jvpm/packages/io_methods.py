f1af8bdbf23aafc0355352c02127304ea02fcdb5
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 23:01:19 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -1,8 +1,6 @@
 from .stack import Stack
 from . import jvpm_methods
 
-S = Stack()
-
 class IoMethods():
     def __init__(self):
         # self.stack = Stack()
@@ -12,15 +10,15 @@ class IoMethods():
         """input from your keyboard"""
         A = jvpm_methods.OpCodeMethods()
         var1 = int(input("What's your first variable? "))
-        S.push(var1)
+        jvpm_methods.S.push(var1)
         print("Your first variable is: " + str(var1))
         var2 = int(input("What's your second variable? "))
-        S.push(var2)
+        jvpm_methods.S.push(var2)
         print("Your second variable is: " + str(var2))
         
         print("Stand back, I will now add " + str(var1) + " + " + str(var2) + ".")
         A.iadd()
         sum = var1 + var2
         print(str(var1) + " + " + str(var2) + " = ",str(sum))
-        print(S.peek())
+        print(jvpm_methods.S.peek())
         print()
