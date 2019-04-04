61572b6fb142a231a71ac2c2048d7af73436dc0e
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 22:47:56 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -8,6 +8,7 @@ class IoMethods():
         var1 = int(input("What's your first variable? "))
         print("Your first variable is: " + str(var1))
         var2 = int(input("What's your second variable? "))
+        print("Your second variable is: " + str(var2))
         print("Stand back, I will now add " + str(var1) + " + " + str(var2) + ".")
         sum = var1 + var2
         print(str(var1) + " + " + str(var2) + " = ",str(sum))
