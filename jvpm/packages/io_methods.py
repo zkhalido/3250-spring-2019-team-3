e38a1cb5f7ce596f32fa94e2ca88d149bff214f4
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 23:57:30 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -8,7 +8,27 @@ class IoMethods():
       
     def keyboard_add(self, argument):
         """input from your keyboard"""
+        # A = jvpm_methods.OpCodeMethods()
+        # 3 dictionaries based on the opcode passed from the main. 
+        # 1 dict for the method, 1 for the operation, 1 for the sign.
+#         io_dict = {"iadd": A.iadd, "isub": A.isub, "imul": A.imul, "idiv": A.idiv}
+#         method = io_dict.get(argument, "invalid")
+        math_dict = {"iadd": "add", "isub": "subtract", "imul": "multiply", "idiv": "divide"}
+        math = math_dict.get(argument, "invalid")
+#         sign_dict = {"iadd": "+", "isub": "-", "imul": "*", "idiv": "/"}
+#         sign = sign_dict.get(argument, "invalid")
+        
+        print("We will " + " " + math + " " + " your two inputs."
+              
+        #use A to call the math method.
         A = jvpm_methods.OpCodeMethods()
+        var1 = int(input("What's your first variable? "))
+        jvpm_methods.S.push(var1)
+        print("Your first variable is: " + str(var1))
+        var2 = int(input("What's your second variable? "))
+        jvpm_methods.S.push(var2)
+        print("Your second variable is: " + str(var2))
+        
         # 3 dictionaries based on the opcode passed from the main. 
         # 1 dict for the method, 1 for the operation, 1 for the sign.
         io_dict = {"iadd": A.iadd, "isub": A.isub, "imul": A.imul, "idiv": A.idiv}
@@ -17,26 +37,6 @@ class IoMethods():
         math = math_dict.get(argument, "invalid")
         sign_dict = {"iadd": "+", "isub": "-", "imul": "*", "idiv": "/"}
         sign = sign_dict.get(argument, "invalid")
-        
-        print("We will " + " " + math + " " + " your two inputs."
-              
-        #use A to call the math method.
-        # A = jvpm_methods.OpCodeMethods()
-        var1 = int(input("What's your first variable? "))
-        jvpm_methods.S.push(var1)
-        print("Your first variable is: " + str(var1))
-        var2 = int(input("What's your second variable? "))
-        jvpm_methods.S.push(var2)
-        print("Your second variable is: " + str(var2))
-        
-#         # 3 dictionaries based on the opcode passed from the main. 
-#         # 1 dict for the method, 1 for the operation, 1 for the sign.
-#         io_dict = {"iadd": A.iadd, "isub": A.isub, "imul": A.imul, "idiv": A.idiv}
-#         method = io_dict.get(argument, "invalid")
-#         math_dict = {"iadd": "add", "isub": "subtract", "imul": "multiply", "idiv": "divide"}
-#         math = math_dict.get(argument, "invalid")
-#         sign_dict = {"iadd": "+", "isub": "-", "imul": "*", "idiv": "/"}
-#         sign = sign_dict.get(argument, "invalid")
         
         print("Stand back, I will now " + math + " " + str(var1) + " " + sign + " " + str(var2) + ".")
         method()
