38bc1cc9195ab109e4c9374251025286e033889d
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 22:54:18 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -5,17 +5,17 @@ class IoMethods():
       
     def keyboard_in(self):
         """input from your keyboard"""
-        A = packages.jvpm_methods.OpCodeMethods()
+        A = jvpm_methods.OpCodeMethods()
         var1 = int(input("What's your first variable? "))
-        packages.jvpm_methods.S.push(var1)
+        jvpm_methods.S.push(var1)
         print("Your first variable is: " + str(var1))
         var2 = int(input("What's your second variable? "))
-        packages.jvpm_methods.S.push(var2)
+        jvpm_methods.S.push(var2)
         print("Your second variable is: " + str(var2))
         
         print("Stand back, I will now add " + str(var1) + " + " + str(var2) + ".")
         A.iadd()
         sum = var1 + var2
         print(str(var1) + " + " + str(var2) + " = ",str(sum))
-        print(packages.jvpm_methods.S.peek())
+        print(jvpm_methods.S.peek())
         print()
