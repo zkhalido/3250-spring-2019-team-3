328d7582087a161070ac83fa445a831904211f63
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-04 00:17:21 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -19,25 +19,12 @@ class IoMethods():
         sign = sign_dict.get(argument, "invalid")
         
         print("We will " + math + " your two inputs from the keyboard.")
-              
-        #use A to call the math method.
-        # A = jvpm_methods.OpCodeMethods()
         var1 = int(input("What's your first variable? "))
         jvpm_methods.S.push(var1)
-        print("Your first variable is: " + str(var1))
+        print("Your first variable is pushed to the stack: " + str(var1))
         var2 = int(input("What's your second variable? "))
         jvpm_methods.S.push(var2)
-        print("Your second variable is: " + str(var2))
-        
-        # 3 dictionaries based on the opcode passed from the main. 
-        # 1 dict for the method, 1 for the operation, 1 for the sign.
-#         io_dict = {"iadd": A.iadd, "isub": A.isub, "imul": A.imul, "idiv": A.idiv}
-#         method = io_dict.get(argument, "invalid")
-#         math_dict = {"iadd": "add", "isub": "subtract", "imul": "multiply", "idiv": "divide"}
-#         math = math_dict.get(argument, "invalid")
-#         sign_dict = {"iadd": "+", "isub": "-", "imul": "*", "idiv": "/"}
-#         sign = sign_dict.get(argument, "invalid")
-        
+        print("Your second variable is pushed to the stack: " + str(var2))
         print("Stand back, I will now " + math + " " + str(var1) + " " + sign + " " + str(var2) + ".")
         method()
         print(str(var1) + " " + sign + " " + str(var2) + " = " + str(jvpm_methods.S.peek()))
