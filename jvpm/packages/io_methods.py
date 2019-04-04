8434cacf51042d22e75ee95199f49a926d85d2bc
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 22:17:16 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -1,4 +1,11 @@
-name = input("What's your name? ")
-print("Nice to meet you " + name + "!")
-age = input("Your age? ")
-print("So, you are already " + age + " years old, " + name + "!")
+class IoMethods():
+  def __init__(self):
+        # self.stack = Stack()
+      
+  def keyboard_in(self):
+    """input from your keyboard"""
+    name = input("What's your name? ")
+    print("Nice to meet you " + name + "!")
+    age = input("Your age? ")
+    print("So, you are already " + age + " years old, " + name + "!")
+    
