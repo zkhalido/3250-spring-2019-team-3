841a9142a4f49662ca33fe2a14208ca62ac01d6d
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 23:35:47 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -18,7 +18,7 @@ class IoMethods():
         print("Your second variable is: " + str(var2))
         
         io_dict = {"iadd": A.iadd}
-        #method = io_dict.get(argument, "invalid")
+        method = io_dict.get(argument, "invalid")
         
         print("Stand back, I will now add " + str(var1) + " + " + str(var2) + ".")
         A.iadd()
