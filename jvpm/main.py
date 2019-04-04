d2110cdf7316b4a99a9083bfef0a67232556ba0b
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 23:08:44 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -42,11 +42,6 @@ if '__main__' == __name__:                  #pragma: no cover
     # **********************************************************************************************
 
     print('\n4) ___Print Hello World!:___\n')
-    
-    packages.jvpm_methods.S.push(1)
-    print("TESTING: Using the main to push then print " + str(packages.jvpm_methods.S.peek()) +
-          " from the stack... yay it works.")
-
 
 
     # **********************************************************************************************
