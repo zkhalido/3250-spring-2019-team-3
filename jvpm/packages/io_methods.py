ecf8972672cc90d5e65317eeabd3e78adb7d3361
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-03 23:51:17 -0600

    Update io_methods.py

--- jvpm/packages/io_methods.py
+++ jvpm/packages/io_methods.py
@@ -17,18 +17,16 @@ class IoMethods():
         jvpm_methods.S.push(var2)
         print("Your second variable is: " + str(var2))
         
+        # 3 dictionaries based on the opcode passed from the main. 
+        # 1 dict for the method, 1 for the operation, 1 for the sign.
         io_dict = {"iadd": A.iadd, "isub": A.isub, "imul": A.imul, "idiv": A.idiv}
         method = io_dict.get(argument, "invalid")
         math_dict = {"iadd": "add", "isub": "subtract", "imul": "multiply", "idiv": "divide"}
         math = math_dict.get(argument, "invalid")
-        print(math)
         sign_dict = {"iadd": "+", "isub": "-", "imul": "*", "idiv": "/"}
         sign = sign_dict.get(argument, "invalid")
-        print(sign)
         
-        print("Stand back, I will now " + math + " " + str(var1) + " + " + str(var2) + ".")
+        print("Stand back, I will now " + math + " " + str(var1) + " " + sign + " " + str(var2) + ".")
         method()
         print(str(var1) + " " + sign + " " + str(var2) + " = " + str(jvpm_methods.S.peek()))
-        
-        # A.method()
         print()
