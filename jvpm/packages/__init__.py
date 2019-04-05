476b16def08c8861dd6b9a91d333172633a7c8eb
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 14:59:16 -0600

    Update __init__.py

--- jvpm/packages/__init__.py
+++ jvpm/packages/__init__.py
@@ -1,2 +1,2 @@
 print(f'Invoking __init__.py for {__name__}')
-from . import jvpm_dict, jvpm_methods, pool_methods, pool_translate, stack, io_methods
+from . import jvpm_dict, jvpm_methods, pool_methods, pool_translate, stack
