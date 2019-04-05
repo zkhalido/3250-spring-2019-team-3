4e0ef7ef94f59f4c5ebd94a717a28fdb5d77be60
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 15:22:54 -0600

    Update jvpm_methods.py

--- jvpm/packages/jvpm_methods.py
+++ jvpm/packages/jvpm_methods.py
@@ -17,7 +17,7 @@ class OpCodeMethods():
         var2 = numpy.int32(S.pop())
         var1 = numpy.int32(S.pop())
         S.push(var1 + var2)
-        # self.println()
+        self.println()
         
     def invokevirtual(self):
         """gets method from constant pool and calls it"""
