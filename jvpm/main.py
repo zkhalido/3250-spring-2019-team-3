9209ba6d61f13025ff3a87de7bff57066a5a4162
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 17:57:38 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -32,7 +32,7 @@ if '__main__' == __name__:                  #pragma: no cover
 
     # **********************************************************************************************
     
-    print('\n___Implement translated method Opcodes from .class file.'
+    print('\n___Implement method Opcodes from .class file.'
           ' Take input from the keybaord:___')
 
     packages_opcodes = packages.jvpm_opcodes.OpCodes()
