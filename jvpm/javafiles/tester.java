8a92764a16d9cf4994f1dfb2a10427d4ccd1ab1e
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 21:09:51 -0600

    Update tester.java

--- jvpm/javafiles/tester.java
+++ jvpm/javafiles/tester.java
@@ -1,10 +1,6 @@
 class tester {
     static void main(String[] args) { 
         int a = 2;
-        a++;
-        
-        def print(self):
-            print("test")
-        
+        a++;        
     };
 }
