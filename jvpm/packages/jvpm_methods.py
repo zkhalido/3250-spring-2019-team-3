0641adfae62836597b9a8e9a195afab1e76f343c
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 00:13:59 -0600

    Update jvpm_methods.py

--- jvpm/packages/jvpm_methods.py
+++ jvpm/packages/jvpm_methods.py
@@ -28,7 +28,7 @@ class OpCodeMethods():
     def get_iv_const(self):
         for i in jvpm_opcodes.inv_virt_const:
             print(jvpm_opcodes.inv_virt_const[i])
-            i++;
+            i = i + 1;
         return
 
     def invokevirtual(self):
