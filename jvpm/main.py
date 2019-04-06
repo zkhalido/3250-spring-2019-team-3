13c7ae53fcdc876dbf67d6b0e5967ca89433b002
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 22:46:36 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -40,7 +40,7 @@ if '__main__' == __name__:                  #pragma: no cover
           ' Take input from the keybaord:___\n')
 
     packages.jvpm_opcodes.OpCodes().dict_search()
-    print(packages.jvpm_opcodes.inv_virt_const)
+    print(packages.jvpm_opcodes.inv_virt_const[0])
     
 
     # **********************************************************************************************
