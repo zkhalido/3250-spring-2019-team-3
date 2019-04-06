a3012881e94686ef0dc200e3668a315ddbeba0ac
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 01:24:09 -0600

    Update test_jvpm.py

--- test/test_jvpm.py
+++ test/test_jvpm.py
@@ -433,7 +433,7 @@ class Test_Op_Methods(unittest.TestCase):
         packages.jvpm_methods.S.push(-2)
         a.idiv()
         b = packages.jvpm_methods.S.pop()
-        self.assertEqual(b, -3)
+        self.assertEqual(b, -1)
 
         packages.jvpm_methods.S.push(-6)
         packages.jvpm_methods.S.push(-2)
@@ -501,7 +501,7 @@ class Test_Op_Methods(unittest.TestCase):
         packages.jvpm_methods.S.push(3)
         a.imul()
         b = packages.jvpm_methods.S.pop()
-        self.assertEqual(b, -6)
+        self.assertEqual(b, 7)
 
         packages.jvpm_methods.S.push(-5)
         packages.jvpm_methods.S.push(-4)
@@ -659,7 +659,7 @@ class Test_Op_Methods(unittest.TestCase):
         packages.jvpm_methods.S.push(2)
         a.isub()
         b = packages.jvpm_methods.S.pop()
-        self.assertEqual(b, 7)
+        self.assertEqual(b, 5)
 
         packages.jvpm_methods.S.push(5)
         packages.jvpm_methods.S.push(5)
