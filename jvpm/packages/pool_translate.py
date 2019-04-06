e699e8517eeff2010a496d294100cc3acb11375c
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 21:54:11 -0600

    Update pool_translate.py

--- jvpm/packages/pool_translate.py
+++ jvpm/packages/pool_translate.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from . import jvpm_opcodes, pool_methods
 
 super_index = 0
-methodrefs = ["test"]
+methodrefs = ["iadd", "println"]
 class PoolTranslate:
 
     def __init__(self):
