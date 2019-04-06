0848d6655d33a458f073cb9ea58ce7b840228478
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 02:08:11 -0600

    Update pool_translate.py

--- jvpm/packages/pool_translate.py
+++ jvpm/packages/pool_translate.py
@@ -7,7 +7,7 @@ class PoolTranslate:
 
     def __init__(self):
         self.dictionary = defaultdict(list)
-        H = jvpm_opcodes.HeaderClass(name = "jvpm/javafiles/tester.class")
+        H = jvpm_opcodes.HeaderClass(name = "jvpm/javafiles/AddTwo.class")
         self.dictionary = H.get_const_pool()
         self.byte_list_count = len(self.dictionary.keys())
         global new_l
