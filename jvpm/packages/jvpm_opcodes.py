b4867833331f4b2591274b03aa962e429b0da466
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 04:51:44 -0600

    Update jvpm_opcodes.py

--- jvpm/packages/jvpm_opcodes.py
+++ jvpm/packages/jvpm_opcodes.py
@@ -126,10 +126,10 @@ class HeaderClass():
                 position += 4
             # Method Ref
             elif self.data[data_offset] == 10:
-                self.temp[i].append(format(self.data[10 + i + position], '02x'))
-                self.temp[i].append(format(self.data[11 + i + position] +
+                temp[i].append(format(self.data[10 + i + position], '02x'))
+                temp[i].append(format(self.data[11 + i + position] +
                                       self.data[12 + i + position], '02x'))
-                self.temp[i].append(format(self.data[13 + i + position] +
+                temp[i].append(format(self.data[13 + i + position] +
                                       self.data[14 + i + position], '02x'))
                 position += 4
             # Interface Method Ref
