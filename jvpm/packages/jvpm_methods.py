8dd2908b99971c0c6487936548193eddb15974ea
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 23:11:27 -0600

    Update jvpm_methods.py

--- jvpm/packages/jvpm_methods.py
+++ jvpm/packages/jvpm_methods.py
@@ -1,7 +1,7 @@
 """Imports"""
 import numpy
 from .stack import Stack
-from . import jvpm_opcodes
+from . import jvpm_opcodes, pool_translate
 
 # ****************************************************************************************
 
