b51ee192206f01d80845a72c19f1c1b0a9597a04
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-05 21:44:58 -0600

    Update pool_translate.py

--- jvpm/packages/pool_translate.py
+++ jvpm/packages/pool_translate.py
@@ -2,10 +2,11 @@ from collections import defaultdict
 from . import jvpm_opcodes, pool_methods
 
 super_index = 0
+methodrefs = []
 class PoolTranslate:
 
     def __init__(self):
-        methodrefs = {}
+        # methodrefs = {}
         self.dictionary = defaultdict(list)
         H = jvpm_opcodes.HeaderClass(name = "jvpm/javafiles/tester.class")
         self.dictionary = H.get_const_pool()
