abbd747df8767e9e71007903927c59a7dc46398d
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 21:05:03 -0600

    Update jvpm_methods.py

--- jvpm/packages/jvpm_methods.py
+++ jvpm/packages/jvpm_methods.py
@@ -46,6 +46,7 @@ class OpCodeMethods():
     def invokevirtual(self):
         """gets method from constant pool and calls it."""
         method = pool_translate.methodrefs[int(jvpm_opcodes.INVOKEVIRTUAL_CONST.popleft())]
+        print("Method called:\n" + method)
         self.token_dict(method)
 
     def next_int(self):
