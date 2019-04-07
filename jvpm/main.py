6707580d9b993e1db71e7669dc973733f02d7272
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 21:46:11 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -32,7 +32,7 @@ if '__main__' == __name__:                  #pragma: no cover
         else:
             print(key, " ", X.token_dict(N[key][0]), "\t\t\t", N[key])
             
-    print("\nStack of translated Methods used by invokevirtual:\n" + str(packages.pool_translate.methodrefs))
+    print("\nInvokevirtual[] stack of translated Methods:\n" + str(packages.pool_translate.methodrefs))
 
     # **********************************************************************************************
     
