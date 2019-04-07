ecf67dc994caed7fd56726d01e251c9d1621123d
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-06 22:04:34 -0600

    Update jvpm_methods.py

--- jvpm/packages/jvpm_methods.py
+++ jvpm/packages/jvpm_methods.py
@@ -53,7 +53,7 @@ class OpCodeMethods():
 
     def next_int(self):
         """receive input from the keyboard."""
-        var1 = numpy.int32(int(input("Enter integer: ")))
+        var1 = numpy.int32(int(input("Enter number: ")))
         S.push(var1)
         
     def get_operation(self):
