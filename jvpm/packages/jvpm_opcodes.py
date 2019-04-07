928d9982df658a54b5aba494e14d54dc0e1a75aa
minouye2 <minouye2@msudenver.edu>
2019-04-07 14:19:51 -0600

    added filepath

--- jvpm/packages/jvpm_opcodes.py
+++ jvpm/packages/jvpm_opcodes.py
@@ -197,6 +197,10 @@ class OpCodes():
         input_file = input("Enter class filename")
         data = []
         if input_file.upper() == "HELLOWORLD":
-            with open("jvpm/javafiles/HelloWorld.class","rb"):
+            with open("jvpm/javafiles/HelloWorld.class",'rb') as binary_file:
+                opcode_bytes = binary_file[336:343]
+                return opcode_bytes
         if input_file.upper() == "TESTPRINTLINE":    
-            with open("jvpm/javafiles/testPrintLine.class","rb"):
+            with open("jvpm/javafiles/testPrintLine.class",'rb') as binary_file:
+                opcode_bytes = binary_file[362:369]
+                return opcode_bytes
