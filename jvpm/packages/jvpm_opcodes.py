41b2cdb9f65e82f64eb7e2176064a5e15c396f3a
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-07 16:27:15 -0600

    Update jvpm_opcodes.py

--- jvpm/packages/jvpm_opcodes.py
+++ jvpm/packages/jvpm_opcodes.py
@@ -198,7 +198,7 @@ class OpCodes():
         data = []
         if input_file.upper() == "HELLOWORLD":
             with open("jvpm/javafiles/HelloWorld.class",'rb') as binary_file:
-                opcode_bytes = binary_file[336:343]
+                opcode_bytes = binary_file #[336:343]
                 self.op_bytes = binary_file.read()
                 print(self.op_bytes)
                 return opcode_bytes
