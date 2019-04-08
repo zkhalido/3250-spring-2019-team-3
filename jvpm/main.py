1c232b37a81363aeb5b0251ecd00bc6fdb5aeb23
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-07 23:19:52 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -48,6 +48,6 @@ if '__main__' == __name__:                  #pragma: no cover
     print("Hello World!")
     # print(packages.jvpm_opcodes.OpCodes.get_opcode_bytes())
     print()
-    print("__________________________") 
+    print("_________________________") 
 
     # **********************************************************************************************
