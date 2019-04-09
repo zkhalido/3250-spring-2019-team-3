8e3d2c878f546b99218a47244b41e498c46d4cce
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-08 20:13:09 -0600

    Update AddWorld.java

--- jvpm/javafiles/AddWorld.java
+++ jvpm/javafiles/AddWorld.java
@@ -1,4 +1,4 @@
-public class AddTwo {
+public class AddWorld {
     public static void main (String[] args) throws java.io.IOException {
         java.util.Scanner reader = new java.util.Scanner(System.in);
         int a = reader.nextInt();
