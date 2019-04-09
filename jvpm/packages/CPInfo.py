0941cebda263026185d3beefcc2a8f163c10a338
Bob Kupfner <azuesflip@gmail.com>
2019-04-08 22:25:47 -0600

    completely revamped the way this is all being pulled.'

--- jvpm/packages/CPInfo.py
+++ jvpm/packages/CPInfo.py
@@ -1,9 +1,9 @@
 from enum import Enum
-from .Unpacker import Unpacker
+from bitstring import ConstBitStream
 import struct
 
-class ConstTag(enum):
-	CLASS = 7
+class ConstTag(Enum):
+    CLASS = 7
     FIELD = 9
     METHOD = 10
     INTERFACE = 11
@@ -21,88 +21,91 @@ class ConstTag(enum):
 
 class ConstInfo:
 
-	def __init__(self):
-		self.tag = null
+    def __init__(self):
+        self.tag = None
 
-	def read(self, n, position):
-		tag = struct.unpack('!B', r.read(1))
+    def read(self, n):
+        tag = n.read('uint:8')
+        self.tag = ConstTag(tag)
 
-		self.tag = ConstTag(tag)
+        if self.tag == ConstTag.CLASS:
+            self.parseClass(n)
+        elif self.tag == ConstTag.FIELD:
+            self.parseField(n)
+        elif self.tag == ConstTag.METHOD:
+            self.parseMethod(n)
+        elif self.tag == ConstTag.INTERFACE:
+            self.parseInterface(n)
+        elif self.tag == ConstTag.STRING:
+            self.parseString(n)
+        elif self.tag == ConstTag.INTEGER:
+            self.parseInteger(n)
+        elif self.tag == ConstTag.FLOAT:
+            self.parseFloat(n)
+        elif self.tag == ConstTag.LONG:
+            self.parseLong(n)
+        elif self.tag == ConstTag.DOUBLE:
+            self.parseDouble(n)
+        elif self.tag == ConstTag.NAME_AND_TYPE:
+            self.parseNameAndType(n)
+        elif self.tag == ConstTag.UTF8:
+            self.parseUTF8(n)
+        elif self.tag == ConstTag.METHOD_HANDLE:
+            self.parseMethodHandle(n)
+        elif self.tag == ConstTag.METHOD_TYPE:
+            self.parseMethodType(n)
+        elif self.tag == ConstTag.INVOKE_DYNAMIC:
+            self.parseInvokeDynamic(n)
 
-		if self.tag == ConstTag.CLASS
-			self.parseClass(n)
-		if self.tag == ConstTag.FIELD
-			self.parseField(n)
-		if self.tag == ConstTag.METHOD
-			self.parseMethod(n)
-		if self.tag == ConstTag.INTERFACE
-			self.parseInterface(n)
-		if self.tag == ConstTag.STRING
-			self.parseString(n)
-		if self.tag == ConstTag.INTEGER
-			self.parseInteger(n)
-		if self.tag == ConstTag.FLOAT
-			self.parseFloat(n)
-		if self.tag == ConstTag.LONG
-			self.parseLong(n)
-		if self.tag == ConstTag.NAME_AND_TYPE
-			self.parseNameAndType(n)
-		if self.tag == ConstTag.UTF8
-			self.parseUTF8(n)
-		if self.tag == ConstTag.METHOD_HANDLE
-			self.parseMethodHandle(n)
-		if self.tag == ConstTag.METHOD_TYPE
-			self.parseMethodType(n)
-		if self.tag == ConstTag.INVOKE_DYNAMIC
-			self.parseInvokeDynamic(n)
+        return self
 
-	def parseClass(self, n):
-		self.name_index = struct.unpack('!H', self.data.read(2))
+    def parseClass(self, n):
+        self.name_index = n.read('uint:16')
 
-	def parseField(self, n):
-		self.class_index = struct.unpack('!H', self.data.read(2))
-		self.name_and_type_index = struct.unpack('!H', self.data.read(2))
+    def parseField(self, n):
+        self.class_index = n.read('uint:16')
+        self.name_and_type_index = n.read('uint:16')
 
-	def parseMethod(self, n):
-		self.class_index = struct.unpack('!H', self.data.read(2))
-		self.name_and_type_index = struct.unpack('!H', self.data.read(2))
+    def parseMethod(self, n):
+        self.class_index = n.read('uint:16')
+        self.name_and_type_index = n.read('uint:16')
 
-	def parseInterface(self, n):
-		self.class_index = struct.unpack('!H', self.data.read(2))
-		self.name_and_type_index = struct.unpack('!H', self.data.read(2))
+    def parseInterface(self, n):
+        self.class_index = n.read('uint:16')
+        self.name_and_type_index = n.read('uint:16')
 
-	def parseString(self, n):
-		self.string_index = struct.unpack('!H', self.data.read(2))
+    def parseString(self, n):
+        self.string_index = n.read('uint:16')
 
-	def parseInteger(self, n):
-		self.bytes = struct.unpack('!I', self.data.read(4))
+    def parseInteger(self, n):
+        self.bytes = struct.unpack('!I', self.data.read(4))
 
-	def parseFloat(self, n):
-		self.bytes = struct.unpack('!I', self.data.read(4))
+    def parseFloat(self, n):
+        self.bytes = struct.unpack('!I', self.data.read(4))
 
-	def parseLong(self, n):
-		self.high_bytes = struct.unpack('!I', self.data.read(4))
-		self.low_bytes = struct.unpack('!I', self.data.read(4))
+    def parseLong(self, n):
+        self.high_bytes = struct.unpack('!I', self.data.read(4))
+        self.low_bytes = struct.unpack('!I', self.data.read(4))
 
-	def parseDouble(self, n):
-		self.high_bytes = struct.unpack('!I', self.data.read(4))
-		self.low_bytes = struct.unpack('!I', self.data.read(4))
+    def parseDouble(self, n):
+        self.high_bytes = struct.unpack('!I', self.data.read(4))
+        self.low_bytes = struct.unpack('!I', self.data.read(4))
 
-	def parseNameAndType(self, n):
-		self.name_index = struct.unpack('!H', self.data.read(2))
-		self.descriptor_index = struct.unpack('!H', self.data.read(2))
+    def parseNameAndType(self, n):
+        self.name_index = n.read('uint:16')
+        self.descriptor_index = n.read('uint:16')
 
-	def parseUTF8(self, n):
-		self.length = struct.unpack('!H', self.data.read(2))
-		self.string = struct.unpack('!s'.format(self.length), self.data.read(self.length))
+    def parseUTF8(self, n):
+        self.length = n.read('uint:16')
+        self.string = n.read('bytes:(%d)' % self.length).decode('utf-8')
 
-	def parseMethodHandle(self, n):
-		self.reference_kind = struct.unpack('!B', r.read(1))
-		self.reference_index = struct.unpack('!H', self.data.read(2))
+    def parseMethodHandle(self, n):
+        self.reference_kind = n.read('uint:8')
+        self.reference_index = n.read('uint:16')
 
-	def parseMethodType(self, n):
-		self.descriptor_index = struct.unpack('!H', self.data.read(2))
+    def parseMethodType(self, n):
+        self.descriptor_index = struct.unpack('!H', self.data.read(2))
 
-	def parseInvokeDynamic(self, n):
-		self.bootstrap_method_attr_index = struct.unpack('!H', self.data.read(2)).decode('utf-8')
-		self.name_and_type_index = struct.unpack('!H', self.data.read(2))
+    def parseInvokeDynamic(self, n):
+        self.bootstrap_method_attr_index = struct.unpack('!H', self.data.read(2)).decode('utf-8')
+        self.name_and_type_index = struct.unpack('!H', self.data.read(2))
--- jvpm/packages/traverse.py
+++ jvpm/packages/traverse.py
@@ -2,6 +2,9 @@
 import unittest
 import stack
 import struct
+from CPInfo import ConstInfo, ConstTag
+from bitstring import ConstBitStream
+
 
 from collections import defaultdict
 # from collections import namedtuple
@@ -12,157 +15,91 @@ from collections import defaultdict
 class HeaderClass():
 
     def __init__(self):
-        with open('../javafiles/testPrintLine.class', 'rb') as binary_file:
-            self.data = binary_file.read()
+            self.data = ConstBitStream(filename = 'tester.class')
 
     def get_magic(self):
-        magic = struct.unpack('!I', self.data.read(4))
+        magic = self.data.read('hex:32')
         return magic
 
     def get_minor(self):
         #print("Minor: ", self.data[4] + self.data[5])
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
 
     def get_major(self):
         #print("Major: ", self.data[6] + self.data[7])
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
 
     def get_const_pool_count(self):
         # print("Contant Pool Count: ", self.data[8] + self.data[9])
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
     
 
     
     def get_const_pool(self):
         
-        constant_pool = []
-        const_pool_count = get_const_pool_count()
-
-        i = 1
-        for i in range(const_pool_count):
-            constants = CPInfo.read(self.data)
-            constant_pool.append(constants)
+        constants_pool = []
+        const_pool_count = self.get_const_pool_count()
+        i = 0
+        for i in range(const_pool_count-1):
+            constant = ConstInfo().read(self.data)
+            constants_pool.append(constant)
+            if constant.tag == ConstTag.DOUBLE or ConstTag.LONG:
+                    constants_pool.append(ConstInfo())
+                    i += 1
 
             i + 1
-        return constant_pool
+        return constants_pool
 
     
 
     def get_access_flags(self):
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
 
     def get_this_class(self):
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
 
     def get_super_class(self):
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
 
     def get_interfaces_count(self):
-        return struct.unpack('!H', self.data.read(2))
+        return self.data.read('uint:16')
     
     
     def get_interfaces(self):
         interface_class_names = []
         count = self.get_interfaces_count()
-        for i in range(count):
-            
+        #for i in range(count):
+
         return interface_class_names
 
     def get_fields_count(self):
-        holder = position + self.get_interfaces_count() + 33
-        #print(format(self.data[holder], '02X'))
-        #print(format(self.data[holder + 1], '02X'))
-        return self.data[holder] + self.data[holder + 1]
+        return
 
     def get_fields(self):
-        temp = []
-        count = self.get_fields_count()
-<<<<<<< HEAD:jvpm/jvpm.py
-        index = position + self.get_interfaces_count() + 33
-        for i in range(count):
-            temp.append(format(self.data[index + i], '02X'))
-        #print("Fields Length: ", len(temp))
-        #print(temp)
-=======
-        index = self.get_const_pool_length() + self.get_interfaces_count() + 18
-        if(count > 0):
-            for i in range(count):
-                 temp.append(format(self.data[index + i], '02X'))
-            print("Fields Length: ", len(temp))
-            print(temp)
-        else:
-            print("No interface table")
->>>>>>> efe2fe216147da0e585c21df952cf5d2af792f4d:jvpm/packages/traverse.py
-        return temp
+        return
 
     def get_methods_count(self):
-        holder = position + self.get_interfaces_count() + self.get_fields_count() + 35
-        #print(format(self.data[holder], '02X'))
-        #print(format(self.data[holder + 1], '02X'))
-        return self.data[holder] + self.data[holder + 1]
+        return
 
     def get_methods(self):
-        temp = []
-        count = self.get_methods_count()
-        index = position + self.get_interfaces_count() + self.get_fields_count() + 35
-        print("index = " + str(index))
-        if(count > 0):
-            for i in range(count):
-                temp.append(format(self.data[index + i], '02X'))
-            print("Methods Length: ", len(temp))
-            print(temp)
-            print(temp[151], temp[152], temp[153], temp[154], temp[155]) # 04, 3c, 84, 01, 01
-        else:
-            print("No Methods")
-        return temp
+        return
 
     def get_attributes_count(self):
-        holder = position + self.get_interfaces_count() + self.get_fields_count() + self.get_methods_count() + 20
-        print(format(self.data[holder], '02X'))
-        print(format(self.data[holder + 1], '02X'))
-        return self.data[holder] + self.data[holder + 1]
+        return
 
     def get_attributes(self):
-        temp = []
-        count = self.get_attributes_count()
-        index = position + self.get_interfaces_count() + self.get_fields_count() + self.get_methods_count() + 22
-        for i in range(count):
-            temp.append(format(self.data[index + i], '02X'))
-        print("Attributes Length: ", len(temp))
-        print(temp)
-        return temp
-
-    def single_part_tuple(self, current):
-        idNum = format(self.data[current], '02x')
-        info = format(self.data[current + 1] + self.data[current + 2], '02x')
-        temp = (idNum, info)
-        return temp
-
-    def single_part_long_tuple(self, current):
-        idNum = format(self.data[current], '02x')
-        info = self.data[11 + i + position] + self.data[12 + i + position] +
-                    self.data[13 + i + position] + self.data[14 + i + position], '02x')
-        temp = (idNum, info)
-        return temp
-
-    def double_part_tuple(self, current):
-        idNum = format(self.data[current], '02x')
-        info = format(self.data[current + 1] + self.data[current + 2], '02x')
-        info2 = format(self.data[current + 3] + self.data[current + 4], '02x')
-        temp = (idNum, info, info2)
-        return temp
+        return
 
 if '__main__' == __name__:
     d = HeaderClass()
     d.get_magic()
     d.get_minor()
     d.get_major()
-    #print(d.get_const_pool())
     d.get_const_pool()
-    d.get_access_flags()
-    d.get_this_class()
-    d.get_super_class()
-    d.get_interfaces_count()
+    print(d.get_access_flags())
+    print(d.get_this_class())
+    print(d.get_super_class())
+    print(d.get_interfaces_count())
     d.get_interfaces()
     d.get_fields_count()
     d.get_fields()
