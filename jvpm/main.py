7ee1e00169b25746f5efe7e8d3059556e2c772b9
chris <cjohn268@msudenver.edu>
2019-04-10 19:56:54 -0600

    added new code to translate still needs more methods but works better and returns array with accurate locations for opcodes to work

--- jvpm/main.py
+++ jvpm/main.py
@@ -3,13 +3,13 @@ import webbrowser
 
 if '__main__' == __name__:                  #pragma: no cover
 
-    H = packages.jvpm_opcodes.HeaderClass()               #pragma: no cover
+    H = packages.jvpm_opcodes.HeaderClass(name = "jvpm/javafiles/AddTwo.class")               #pragma: no cover
     H.get_magic()                   #pragma: no cover
     H.get_minor()                   #pragma: no cover
     H.get_major()                   #pragma: no cover
 
-    P = packages.pool_translate.PoolTranslate("jvpm/javafiles/AddTwo.class")                 #pragma: no cover
-    N = P.translate()                   #pragma: no cover
+    P = packages.pool_translate.PoolTranslate(name ="jvpm/javafiles/AddTwo.class")                 #pragma: no cover
+    N = P.translate_pool()                   #pragma: no cover
     X = packages.pool_methods.TagTranslate()#pragma: no cover
     
     packages.jvpm_opcodes.OpCodes().dict_search()
