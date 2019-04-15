4132e6fe21f2ef7d46dcf43de21deff7a1d4fd08
Azuesflip <azuesflip@gmail.com>
2019-04-15 12:57:14 -0600

    added in new descriptor for bits being taken in as bits instead of n

--- jvpm/packages/CPInfo.py
+++ jvpm/packages/CPInfo.py
@@ -17,95 +17,94 @@ class ConstTag(Enum):
     METHOD_HANDLE = 15
     METHOD_TYPE = 16
     INVOKE_DYNAMIC = 18
-    MODULE = 19
 
 class ConstInfo:
 
     def __init__(self):
         self.tag = None
 
-    def read(self, n):
-        tag = n.read('uint:8')
+    def read(self, bits):
+        tag = bits.read('uint:8')
         self.tag = ConstTag(tag)
 
         if self.tag == ConstTag.CLASS:
-            self.parseClass(n)
+            self.parseClass(bits)
         elif self.tag == ConstTag.FIELD:
-            self.parseField(n)
+            self.parseField(bits)
         elif self.tag == ConstTag.METHOD:
-            self.parseMethod(n)
+            self.parseMethod(bits)
         elif self.tag == ConstTag.INTERFACE:
-            self.parseInterface(n)
+            self.parseInterface(bits)
         elif self.tag == ConstTag.STRING:
-            self.parseString(n)
+            self.parseString(bits)
         elif self.tag == ConstTag.INTEGER:
-            self.parseInteger(n)
+            self.parseInteger(bits)
         elif self.tag == ConstTag.FLOAT:
-            self.parseFloat(n)
+            self.parseFloat(bits)
         elif self.tag == ConstTag.LONG:
-            self.parseLong(n)
+            self.parseLong(bits)
         elif self.tag == ConstTag.DOUBLE:
-            self.parseDouble(n)
+            self.parseDouble(bits)
         elif self.tag == ConstTag.NAME_AND_TYPE:
-            self.parseNameAndType(n)
+            self.parseNameAndType(bits)
         elif self.tag == ConstTag.UTF8:
-            self.parseUTF8(n)
+            self.parseUTF8(bits)
         elif self.tag == ConstTag.METHOD_HANDLE:
-            self.parseMethodHandle(n)
+            self.parseMethodHandle(bits)
         elif self.tag == ConstTag.METHOD_TYPE:
-            self.parseMethodType(n)
+            self.parseMethodType(bits)
         elif self.tag == ConstTag.INVOKE_DYNAMIC:
-            self.parseInvokeDynamic(n)
+            self.parseInvokeDynamic(bits)
 
         return self
 
-    def parseClass(self, n):
-        self.name_index = n.read('uint:16')
+    def parseClass(self, bits):
+        self.name_index = bits.read('uint:16')
 
-    def parseField(self, n):
-        self.class_index = n.read('uint:16')
-        self.name_and_type_index = n.read('uint:16')
+    def parseField(self, bits):
+        self.class_index = bits.read('uint:16')
+        self.name_and_type_index = bits.read('uint:16')
 
-    def parseMethod(self, n):
-        self.class_index = n.read('uint:16')
-        self.name_and_type_index = n.read('uint:16')
+    def parseMethod(self, bits):
+        self.class_index = bits.read('uint:16')
+        self.name_and_type_index = bits.read('uint:16')
 
-    def parseInterface(self, n):
-        self.class_index = n.read('uint:16')
-        self.name_and_type_index = n.read('uint:16')
+    def parseInterface(self, bits):
+        self.class_index = bits.read('uint:16')
+        self.name_and_type_index = bits.read('uint:16')
 
-    def parseString(self, n):
-        self.string_index = n.read('uint:16')
+    def parseString(self, bits):
+        self.string_index = bits.read('uint:16')
 
-    def parseInteger(self, n):
-        self.bytes = struct.unpack('!I', self.data.read(4))
+    def parseInteger(self, bits):
+        self.bytes = bits.read(4))
 
-    def parseFloat(self, n):
-        self.bytes = struct.unpack('!I', self.data.read(4))
+    def parseFloat(self, bits):
+        self.bytes = bits.read(4))
 
-    def parseLong(self, n):
-        self.high_bytes = struct.unpack('!I', self.data.read(4))
-        self.low_bytes = struct.unpack('!I', self.data.read(4))
+    def parseLong(self, bits):
+        self.high_bytes = bits.read(4))
+        self.low_bytes = bits.read(4))
 
-    def parseDouble(self, n):
-        self.high_bytes = struct.unpack('!I', self.data.read(4))
-        self.low_bytes = struct.unpack('!I', self.data.read(4))
+    def parseDouble(self, bits):
+        self.high_bytes = bits.read(4))
+        self.low_bytes = bits.read(4))
 
-    def parseNameAndType(self, n):
-        self.name_index = n.read('uint:16')
-        self.descriptor_index = n.read('uint:16')
+    def parseNameAndType(self, bits):
+        self.name_index = bits.read('uint:16')
+        self.descriptor_index = bits.read('uint:16')
 
-    def parseUTF8(self, n):
-        self.length = n.read('uint:16')
-        self.string = n.read('bytes:(%d)' % self.length).decode('utf-8')
+    def parseUTF8(self, bits):
+        self.length = bits.read('uint:16')
+        self.string = bits.read('bytes:(%d)' % self.length).decode('utf-8')
 
-    def parseMethodHandle(self, n):
-        self.reference_kind = n.read('uint:8')
-        self.reference_index = n.read('uint:16')
+    def parseMethodHandle(self, bits):
+        self.reference_kind = bits.read('uint:8')
+        self.reference_index = bits.read('uint:16')
 
-    def parseMethodType(self, n):
-        self.descriptor_index = struct.unpack('!H', self.data.read(2))
+    def parseMethodType(self, bits):
+        self.descriptor_index = bits.read('uint:16')
 
-    def parseInvokeDynamic(self, n):
-        self.bootstrap_method_attr_index = struct.unpack('!H', self.data.read(2)).decode('utf-8')
-        self.name_and_type_index = struct.unpack('!H', self.data.read(2))
+    def parseInvokeDynamic(self, bits):
+        self.bootstrap_method_attr_index = bits.read('uint:16')
+        self.name_and_type_index = bits.read('uint:16')
