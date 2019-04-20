a0422b004550e6afe0c6301b0d9c56b747e34390
Chris Johnson <43618289+Cjohnson187@users.noreply.github.com>
2019-04-20 12:26:10 -0600

    removed a bunch of stuff to make merge easier

--- jvpm/javafiles/test1.java
+++
@@ -1,7 +0,0 @@
-class test1 {
-    static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = ((((a + b) * b) / b) - b);
-    };
-}
--- jvpm/javafiles/tester.java
+++
@@ -1,6 +0,0 @@
-class tester { 
-  static void main(String[] args) { 
-    int a = 2; 
-    a++; 
-  }; 
-}
