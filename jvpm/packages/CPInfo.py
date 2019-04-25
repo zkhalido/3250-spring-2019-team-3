193c1a552644b1792addb4daddec3e11112e44e0
zkhalido <zkhalido@msudenver.edu>
2019-04-24 23:59:42 -0600

    refactored CPInfo to use dictionary

--- jvpm/packages/CPInfo.py
+++ jvpm/packages/CPInfo.py
@@ -1,22 +1,5 @@
-from enum import Enum
 from bitstring import ConstBitStream
 import struct
-
-class ConstTag(Enum):
-    CLASS = "07"
-    FIELD = "09"
-    METHOD = "0a"
-    INTERFACE = "0b"
-    STRING = "08"
-    INTEGER = "03"
-    FLOAT = "04"
-    LONG = "05"
-    DOUBLE = "06"
-    NAME_AND_TYPE = "0c"
-    UTF8 = "01"
-    METHOD_HANDLE = "0f"
-    METHOD_TYPE = "10"
-    INVOKE_DYNAMIC = "12"
 
 class ConstInfo:
 
@@ -28,36 +11,9 @@ class ConstInfo:
         #pool = []
         tag = bits.read('hex:8')
         self.pool.append(tag)
-        self.tag = ConstTag(tag)
 
-        if self.tag == ConstTag.CLASS:
-            self.parseClass(bits)
-        elif self.tag == ConstTag.FIELD:
-            self.parseField(bits)
-        elif self.tag == ConstTag.METHOD:
-            self.parseMethod(bits)
-        elif self.tag == ConstTag.INTERFACE:
-            self.parseInterface(bits)
-        elif self.tag == ConstTag.STRING:
-            self.parseString(bits)
-        elif self.tag == ConstTag.INTEGER:
-            self.parseInteger(bits)
-        elif self.tag == ConstTag.FLOAT:
-            self.parseFloat(bits)
-        elif self.tag == ConstTag.LONG:
-            self.parseLong(bits)
-        elif self.tag == ConstTag.DOUBLE:
-            self.parseDouble(bits)
-        elif self.tag == ConstTag.NAME_AND_TYPE:
-            self.parseNameAndType(bits)
-        elif self.tag == ConstTag.UTF8:
-            self.parseUTF8(bits)
-        elif self.tag == ConstTag.METHOD_HANDLE:
-            self.parseMethodHandle(bits)
-        elif self.tag == ConstTag.METHOD_TYPE:
-            self.parseMethodType(bits)
-        elif self.tag == ConstTag.INVOKE_DYNAMIC:
-            self.parseInvokeDynamic(bits)
+        self.const_tag_dict[tag](self, bits)
+
         return self.pool
 
     def parseClass(self, bits):
@@ -65,9 +21,6 @@ class ConstInfo:
             next_byte = bits.read('hex:8')
             if (next_byte != "00"):
                 self.pool.append(next_byte)
-
-
-
 
     def parseField(self, bits):
         for i in range(4):
@@ -150,3 +103,20 @@ class ConstInfo:
             next_byte = bits.read('hex:8')
             if (next_byte != "00"):
                 self.pool.append(next_byte)
+
+    const_tag_dict = {
+        "07" : parseClass,
+        "09" : parseField,
+        "0a" : parseMethod,
+        "0b" : parseInterface,
+        "08" : parseString,
+        "03" : parseInteger,
+        "04" : parseFloat,
+        "05" : parseLong,
+        "06" : parseDouble,
+        "0c" : parseNameAndType,
+        "01" : parseUTF8,
+        "0f" : parseMethodHandle,
+        "10" : parseMethodType,
+        "12" : parseInvokeDynamic,
+    }
