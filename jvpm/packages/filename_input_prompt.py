5715e434ff1c4a3fdc58abd015079b652b14c43a
Dominick Licciardi <dominick.nola@gmail.com>
2019-04-29 18:47:46 -0600

    Update filename_input_prompt.py

--- jvpm/packages/filename_input_prompt.py
+++ jvpm/packages/filename_input_prompt.py
@@ -1,5 +1,8 @@
+"""Module that will prompt for file info."""
 class FileNameInputPrompt:
+    """Class to prompt fro info."""
     def input_prompt():
+        """Method that implements the prompt."""
         name = str(input("What file do you want to run: "))
         extension = ".class"
         directory = "jvpm/javafiles/"
