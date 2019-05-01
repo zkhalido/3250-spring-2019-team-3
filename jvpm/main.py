da3944353463337b7a95b3d3ef583ff83e3191a3
Dominick Licciardi <dominick.nola@gmail.com>
2019-05-01 15:04:55 -0600

    Update main.py

--- jvpm/main.py
+++ jvpm/main.py
@@ -24,36 +24,36 @@ if '__main__' == __name__:                  #pragma: no cover
     # print(header_class_object.get_major())
 
     n = header_class_object.get_const_pool()
-    # print(n)
+    print(n)
     p_translator = packages.pool_translate.PoolTranslate(n, header_class_object.skips_in_constant_pool, name = file_name)
 
     pool = p_translator.translate_pool()
-    # print(pool, "translated")
+    print(pool, "translated")
 
     access_flags = header_class_object.get_access_flags()
-    # print(access_flags, "   access flags")
+    print(access_flags, "   access flags")
 
     this_class = header_class_object.get_this_class()
-    # print(this_class, "   this class")
+    print(this_class, "   this class")
 
     super_class = header_class_object.get_super_class()
-    # print(super_class, "   super class")
+    print(super_class, "   super class")
 
     d = header_class_object.get_interfaces_count()
-    # print(d, "   interfaces count")
+    print(d, "   interfaces count")
 
     header_class_object.get_interface() # no method built yet but should just be index in constant pool
 
     e = header_class_object.get_field_count()
-    # print(e, "   field count")
+    print(e, "   field count")
 
     header_class_object.get_field() # no method built yet but should just be variable table
 
     opcodes = header_class_object.get_methods_count()
-    # print(opcodes, " - methods count       \n ", header_class_object.integer_method_count, " -int meth count")
+    print(opcodes, " - methods count       \n ", header_class_object.integer_method_count, " -int meth count")
 
     opcodes = header_class_object.get_methods(pool)
-    # print("OpCodes:\n ", opcodes,    "** op codes **")
+    print("OpCodes:\n ", opcodes,    "** op codes **")
 
 
     dict_search_object = packages.jvpm_opcodes.OpCodes(opcodes, pool)
