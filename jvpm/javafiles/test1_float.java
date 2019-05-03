044dd59d751df494013761f4297e93c0241faeab
Dominick Licciardi <dominick.nola@gmail.com>
2019-05-03 14:51:21 -0600

    Update test1_float.java

--- jvpm/javafiles/test1_float.java
+++ jvpm/javafiles/test1_float.java
@@ -1,8 +1,8 @@
 class test1_float {
     static void main(String[] args) {
-        double a = 1.2;
-        double b = 2.8;
-        double c = a + b;
+        float a = 1.2f;
+        float b = 2.8f;
+        float c = a + b;
         System.out.println(c);
     };
 }
