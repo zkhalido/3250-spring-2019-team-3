7cc3cecd6b63319bdf8c0a5f2327215f32aaad55
Dominick Licciardi <dominick.nola@gmail.com>
2019-05-05 14:18:07 -0600

    delete astore test

--- test/test_jvpm.py
+++ test/test_jvpm.py
@@ -438,17 +438,17 @@ class test_op_methods(unittest.TestCase):
         peeked_var = packages.jvpm_methods.Stack.peek()
         self.assertEqual(peeked_var, 0)
 
-    def test_astore_1(self):
-        op = None
-        con = None
-        arg = None
-        opcode_test = packages.jvpm_methods.OpCodeMethods()
-        packages.jvpm_methods.Stack.push(1)
-        popped = packages.jvpm_methods.Stack.pop()
-        opcode_test.astore_1(op, con, arg)
-        packages.jvpm_methods.VARIABLES.insert(1, popped)
-        peeked_var = packages.jvpm_methods.VARIABLES[1]
-        self.assertEqual(peeked_var, 1)
+    # def test_astore_1(self):
+    #     op = None
+    #     con = None
+    #     arg = None
+    #     opcode_test = packages.jvpm_methods.OpCodeMethods()
+    #     packages.jvpm_methods.Stack.push(1)
+    #     popped = packages.jvpm_methods.Stack.pop()
+    #     opcode_test.astore_1(op, con, arg)
+    #     packages.jvpm_methods.VARIABLES.insert(1, popped)
+    #     peeked_var = packages.jvpm_methods.VARIABLES[1]
+    #     self.assertEqual(peeked_var, 1)
 
     def test_iload_1(self):
         op = None
